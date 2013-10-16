bff2bb46221f7c0f77100e74673424eae27f75c4
Srinivas <spulagam@gainsight.com>
2013-10-16 15:12:47 +0530

    Added new flags in properties file Added method set Gainsight as a default app in salesforce Added method to load setup data and sample data Modified delete querties to include namespace

--- src/com/gainsight/sfdc/pages/BasePage.java
+++ src/com/gainsight/sfdc/pages/BasePage.java
@@ -1,5 +1,8 @@
 package com.gainsight.sfdc.pages;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import com.gainsight.pageobject.core.TestEnvironment;
 import com.gainsight.pageobject.core.WebPage;
 import com.gainsight.sfdc.accounts.pages.AccountsPage;
@@ -21,18 +24,19 @@ import com.gainsight.sfdc.survey.pages.SurveyBasePage;
  */
 public class BasePage extends WebPage implements Constants {
 	private final String READY_INDICATOR = "//div[@id='userNavButton']";
-    private final String OPPORTUNITIES_TAB="//img[@title='Opportunities']";
-    private final String ALL_TABS="//img[@title='All Tabs']";
-    private final String ACCOUNTS_TAB="//a[@title='Accounts Tab']";
-	public Transactions transactionUtil=new Transactions();
-	public AmountsAndDatesUtil amtDateUtil=new AmountsAndDatesUtil();
+	private final String OPPORTUNITIES_TAB = "//img[@title='Opportunities']";
+	private final String ALL_TABS = "//img[@title='All Tabs']";
+	private final String ACCOUNTS_TAB = "//a[@title='Accounts Tab']";
+	private final String DEFAULT_APP_RADIO = "//td[text()='%s']/following-sibling::td//input[@type='radio']";
+	private final String TAB_SELECT = "//td[contains(@class,'labelCol requiredInput') and contains(.,'%s')]//following-sibling::td//select";
+	public Transactions transactionUtil = new Transactions();
+	public AmountsAndDatesUtil amtDateUtil = new AmountsAndDatesUtil();
 
 	public BasePage login() {
 		field.setTextField("username", TestEnvironment.get().getUserName());
 		field.setTextField("password", TestEnvironment.get().getUserPassword());
 		button.click("Login");
 		wait.waitTillElementPresent(READY_INDICATOR, MIN_TIME, MAX_TIME);
-
 		return this;
 	}
 
@@ -41,17 +45,15 @@ public class BasePage extends WebPage implements Constants {
 		field.setTextField("password", pwd);
 		button.click("Login");
 		wait.waitTillElementPresent(READY_INDICATOR, MIN_TIME, MAX_TIME);
-
 		return this;
 	}
 
 	public BasePage logout() {
 		item.click("userNavButton");
 		item.click("//a[text()='Logout']");
-
 		return this;
 	}
-	
+
 	public void beInMainWindow() {
 		element.switchToMainWindow();
 	}
@@ -76,34 +78,77 @@ public class BasePage extends WebPage implements Constants {
 		item.click("//a[contains(text(),'Survey')]");
 		return new SurveyBasePage();
 	}
-	
+
 	public ChurnPage clickOnChurnTab() {
 		item.click("//a[contains(@title,'Churn Tab')]");
 		return new ChurnPage();
 	}
-	
+
 	public OpportunitiesPage clickOnOpportunitiesTab() {
-		if(!field.isElementPresent(OPPORTUNITIES_TAB)){
-			item.click(ALL_TABS);				
+		if (!field.isElementPresent(OPPORTUNITIES_TAB)) {
+			item.click(ALL_TABS);
 		}
-		item.click(OPPORTUNITIES_TAB);		
+		item.click(OPPORTUNITIES_TAB);
 		return new OpportunitiesPage();
 	}
+
 	public AccountsPage clickOnAccountsTab() {
-		item.click(ACCOUNTS_TAB);		
+		item.click(ACCOUNTS_TAB);
 		return new AccountsPage();
 	}
-	
-	public AdministrationBasepage clickOnAdminTab()
-	{   driver.manage().window().maximize();
+
+	public AdministrationBasepage clickOnAdminTab() {
+		driver.manage().window().maximize();
 		item.click("//a[contains(@title,'Administration')]");
 		return new AdministrationBasepage();
-		
 	}
 
 	public void setFilter(String filterFiledName, String value) {
 		field.setTextByKeys("//input[@name='" + filterFiledName + "']", value);
 	}
 
+	public void setDefaultApplication(String appName) {
+		item.click("userNavButton");
+		link.clickLink("Setup");
+		wait.waitTillElementPresent("//a[text()='Manage Users']", MIN_TIME,
+				MAX_TIME);
+		link.clickLink("Manage Users");
+		link.clickLink("Profiles");
+		wait.waitTillElementPresent("//a[contains(.,'System Administrator')]",
+				MIN_TIME, MAX_TIME);
+		item.click("//td[@id='bodyCell']//tr[contains(.,'System Administrator')]//a[contains(.,'Edit')]");
+		wait.waitTillElementPresent("//input[@title='Save']", MIN_TIME,
+				MAX_TIME);
+		item.click(String.format(DEFAULT_APP_RADIO, appName));
+		field.selectFromDropDown(String.format(TAB_SELECT, "Administration"),
+				"Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "Adoption"), "Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "Churn"), "Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "Customers"), "Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "NPS"), "Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "Retention"), "Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "Survey"), "Default On");
+		field.selectFromDropDown(String.format(TAB_SELECT, "Transactions"), "Default On");
+		item.click("//input[@title='Save']");
+		wait.waitTillElementPresent("//a[contains(.,'System Administrator')]",
+				MIN_TIME, MAX_TIME);
+	}
 
+	public void loadDefaultData() throws URISyntaxException {
+		URI uri=new URI(driver.getCurrentUrl());
+		String hostName="https://"+uri.getHost();
+		driver.get(hostName+"/apex/loadsetupdata");
+		String intializeButton="//input[@value='INITIALIZE']";
+		if(element.isElementPresent(intializeButton)){
+			item.click(intializeButton);
+			wait.waitTillElementDisplayed("loadSetupDatatable", MIN_TIME, MAX_TIME);
+		}
+		driver.get(hostName+"/apex/loadsampledata");
+		String loadButton="//input[@value='Load']";
+		if(element.isElementPresent(loadButton)){
+			item.click(loadButton);
+			wait.waitTillElementDisplayed("loadSetupDatatable", MIN_TIME, MAX_TIME);
+		}
+		
+	}
 }
--- src/com/gainsight/sfdc/tests/BaseTest.java
+++ src/com/gainsight/sfdc/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package com.gainsight.sfdc.tests;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -18,54 +19,75 @@ import com.gainsight.utils.SOQLUtil;
 import com.gainsight.utils.TestDataHolder;
 
 public class BaseTest {
-	protected TestDataHolder testDataLoader=new TestDataHolder();
-	String[] dirs={"testdata","sfdc"};
-	public final String TEST_DATA_PATH_PREFIX=generatePath(dirs);
-    TestEnvironment env=new TestEnvironment();	
-    public SOQLUtil soql =new SOQLUtil();
+	protected TestDataHolder testDataLoader = new TestDataHolder();
+	String[] dirs = { "testdata", "sfdc" };
+	public final String TEST_DATA_PATH_PREFIX = generatePath(dirs);
+	TestEnvironment env = new TestEnvironment();
+	public SOQLUtil soql = new SOQLUtil();
 	public BasePage basepage;
-	private final String DELTE_TRANS_LINES="Select id from TransLines__c";
-	private final String DELTE_TRANS_HEADERS="Select id from TransHeader__c";	
-	private final String DELTE_CUSTOMERS="Select id from CustomerInfo__c";
-	
+	private final String DELETE_RECORDS = "Select id from TransHeader__c | Select id from CustomerInfo__c";
+	private final String DELETE_RECORDS_NAMESPACE = "Select id from JBCXM__TransHeader__c | Select id from JBCXM__CustomerInfo__c";
+
 	@BeforeSuite
-	public void init(){		
+	public void init() throws Exception {
 		env.start();
-		soql.deleteQuery(DELTE_TRANS_LINES);
-		soql.deleteQuery(DELTE_TRANS_HEADERS);		
-		soql.deleteQuery(DELTE_CUSTOMERS);
-		env.launchBrower();
-		basepage = new BasePage();
+		try {
+			String deleteFlag = env.getProperty("deleteRecords");
+			String namesapce = env.getProperty("managedPackage");
+			String setAsDefaultApp = env.getProperty("setAsDefaultApp");
+			String loadDefaultData = env.getProperty("loadDefaultData");
+			if (deleteFlag != null && deleteFlag.equals("true")) {
+				if (namesapce != null && namesapce.equals("true"))
+					soql.deleteQuery(DELETE_RECORDS_NAMESPACE);
+				else
+					soql.deleteQuery(DELETE_RECORDS);
+			}
+			env.launchBrower();
+			basepage = new BasePage();
+			if (setAsDefaultApp != null && setAsDefaultApp.equals("true")) {
+				basepage.login();
+				basepage.setDefaultApplication("JBara");
+				basepage.logout();
+			}
+			if (loadDefaultData != null && loadDefaultData.equals("true")) {
+				basepage.login();
+				basepage.loadDefaultData();
+				basepage.logout();
+			}
+		} catch (Exception e) {
+			env.stop();
+			throw e;
+		}
 	}
-	
+
 	@AfterSuite
-	public void fini(){
+	public void fini() {
 		env.stop();
 	}
-	
+
 	@BeforeTest
-	public void beInMainWindow(){
-		basepage.beInMainWindow();		
+	public void beInMainWindow() {
+		basepage.beInMainWindow();
 	}
-	
-	public String generatePath(String[] dirs){
-		String path="";
-		for(String dir : dirs){
-			path=path+dir+"/";			
+
+	public String generatePath(String[] dirs) {
+		String path = "";
+		for (String dir : dirs) {
+			path = path + dir + File.pathSeparator;
 		}
 		return path;
 	}
-	
-	public String currencyFormat(String amt){
+
+	public String currencyFormat(String amt) {
 		DecimalFormat moneyFormat = new DecimalFormat("$###,###");
 		return moneyFormat.format(new Long(amt)).replace("$", "$ ");
 	}
-	public String getCurrentDate(){
-	    Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-       return sdf.format(cal.getTime());
-     
-}
+
+	public String getCurrentDate() {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+		return sdf.format(cal.getTime());
+	}
 
 	public String getFormattedDate(String dateStr) throws ParseException {
 		DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
@@ -73,36 +95,37 @@ public class BaseTest {
 		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
 		return sdf.format(date);
 	}
-	public String getFormattedDate(String dateStr, int days) throws ParseException{
+
+	public String getFormattedDate(String dateStr, int days)
+			throws ParseException {
 		DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-		Date date=formatter.parse(dateStr);	
+		Date date = formatter.parse(dateStr);
 		Calendar c = Calendar.getInstance();
-		c.setTime(date); 
-		c.add(Calendar.DATE, 1); 
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-       return sdf.format(c.getTime());
-     
-}
-	public HashMap<String, String> getMapFromData(String data){
+		c.setTime(date);
+		c.add(Calendar.DATE, 1);
+		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+		return sdf.format(c.getTime());
+	}
+
+	public HashMap<String, String> getMapFromData(String data) {
 		HashMap<String, String> hm = new HashMap<String, String>();
 		System.out.println(data);
-		String[] dataArray=data.substring(1, data.length()-1).split("\\|");
-		for (String record: dataArray){
-			if(record!= null){
-			System.out.println(record);
-			String[] pair=record.split("\\:");
-			hm.put(pair[0], pair[1]);
+		String[] dataArray = data.substring(1, data.length() - 1).split("\\|");
+		for (String record : dataArray) {
+			if (record != null) {
+				System.out.println(record);
+				String[] pair = record.split("\\:");
+				hm.put(pair[0], pair[1]);
 			}
-			
 		}
 		return hm;
 	}
-	public Double calcMRR(int ASV){
-		return Math.ceil(ASV / 12.0);		
+
+	public Double calcMRR(int ASV) {
+		return Math.ceil(ASV / 12.0);
 	}
-	
-	public Double calcARPU(int ASV,int users){
-		return Math.ceil((ASV/12.0)/users);	
+
+	public Double calcARPU(int ASV, int users) {
+		return Math.ceil((ASV / 12.0) / users);
 	}
-	
 }
