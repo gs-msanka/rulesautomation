b18fb88f07a2ff8e67f275cfe0642b2c6962a419
gs-golla <ggolla@gainsight.com>
2013-10-25 17:38:33 +0530

    Retention tab completely implemented.

---
+++ src/com/gainsight/sfdc/retention/pages/RetentionBasePage.java
@@ -0,0 +1,46 @@
+package com.gainsight.sfdc.retention.pages;
+
+import com.gainsight.sfdc.pages.BasePage;
+
+public class RetentionBasePage extends BasePage{
+	private final String READY_INDICATOR = "//a[@title='Retention Tab - Selected']";
+	private final String ALERTS_TAB = "//div[@class='ge-tabbar']/a[text()='Alerts']";
+	private final String EVENTS_TAB = "//div[@class='ge-tabbar']/a[text()='Events']";
+	private final String PLAYBOOKS_TAB = "//div[@class='ge-tabbar']/a[text()='Playbooks']";
+	
+	
+	public RetentionBasePage() {
+		wait.waitTillElementPresent(READY_INDICATOR, MIN_TIME, MAX_TIME);
+	}
+	
+	/**
+	 * Clicks on alerts sub tab under retention tab.
+	 * @return
+	 */
+	public AlertsPage clickOnAlertsTab() {
+		item.click(ALERTS_TAB);
+		return new AlertsPage();
+	}
+	
+	/**
+	 * Clicks on events sub tab under retention tab.
+	 * @return
+	 */
+	public EventsPage clickOnEventsTab() {
+		//driver.manage().window().maximize();
+		item.click(EVENTS_TAB);
+		return new EventsPage();
+	}
+	
+	/**
+	 * Clicks on playbooks sub tab under retention tab.
+	 * @return
+	 */
+	public PlayBooksPage clickOnAnalyticsTab() {
+		item.click(PLAYBOOKS_TAB);
+		return new PlayBooksPage();
+	}
+	
+	
+	
+}
