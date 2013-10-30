97934a3078da0f0c4711a42ed069d8665df396c5
gs-golla <ggolla@gainsight.com>
2013-10-30 23:17:34 +0530

    Playbook page changes.

--- src/com/gainsight/sfdc/retention/pages/PlayBooksPage.java
+++ src/com/gainsight/sfdc/retention/pages/PlayBooksPage.java
@@ -13,6 +13,25 @@ import com.gainsight.sfdc.pages.BasePage;
 public class PlayBooksPage extends BasePage {
 
     private final String READY_INDICATOR = "//div[@class='ga-addPlaybookIcon btn']";
+    private final String DELETE_PLAYBOOK = "//a[@title='Delete playbook']";
+    private final String ADD_PLAYBOOK_BUTTON = "//div[@class='ga-addPlaybookIcon btn']";
+    private final String ISALERT_PLAYBOOK_CHECK = "ga-alert";
+    private final String ISEVENT_PLAYBOOK_CHECK = "ga-event";
+    private final String PBNAME_INPUT = "//input[@class='ga-playbookName pbInputVal']";
+    private final String PBDES_INPTU  = "//textarea[@class='ga-playbookDes pbInputVal']";
+    private final String TASK_SUB_INPUT = "//input[@class='Subject__cInputCls taskParamControlDataInput']";
+    private final String TASK_RELDATECOUNT_INPUT = "//input[@class='Date__cInputCls taskParamControlDataInput']";
+    private final String TASK_PRIORITY_SELECT = "//select[@class='Priority__cInputCls taskParamControlDataInput']";
+    private final String TASK_STATUS_SELECT = "//select[@class='Status__cInputCls taskParamControlDataInput']";
+    private final String CREATE_BUTTON = "//input[@class='ga-btnPrimary ga-savePlaybook' and @type='button' and @value='Create']";
+    private final String CANCEL_BUTTON = "//input[@class='ga-btnSecondary ga-cancelPlaybook' and @type='button' and @value ='Cancel']";
+    private final String UPDATE_BUTTON = "//input[@class='ga-btnPrimary ga-savePlaybook' and @value='Update']";
+    private final String ADD_TASK_BUTTON = "ga-addTaskIconId";
+    private final String TASK_SERCH_INPUT = "//input[@class='ga-searchInput ga-searchTasks'";
+    private final String TASK_SEARCH_CLEAR_BUTTON = "//input[@class='ga-clearBtn ga-searchTaskClearBtn']";
+    private final String PB_EDIT_LINK =  "//a[@class='ga-icn ga-edit ga-editPlaybook']";
+
+
 
     /*
      * Constructor wait for the first element to be displayed in the page.
@@ -325,26 +344,4 @@ public class PlayBooksPage extends BasePage {
         Report.logInfo("Finished verifying playbook & resurting result :" +result);
         return result;
     }
-
-
-    private final String DELETE_PLAYBOOK = "//a[@title='Delete playbook']";
-    private final String ADD_PLAYBOOK_BUTTON = "//div[@class='ga-addPlaybookIcon btn']";
-    private final String ISALERT_PLAYBOOK_CHECK = "ga-alert";
-    private final String ISEVENT_PLAYBOOK_CHECK = "ga-event";
-    private final String PBNAME_INPUT = "//input[@class='ga-playbookName pbInputVal']";
-    private final String PBDES_INPTU  = "//textarea[@class='ga-playbookDes pbInputVal']";
-    private final String TASK_SUB_INPUT = "//input[@class='Subject__cInputCls taskParamControlDataInput']";
-    private final String TASK_RELDATECOUNT_INPUT = "//input[@class='Date__cInputCls taskParamControlDataInput']";
-    private final String TASK_PRIORITY_SELECT = "//select[@class='Priority__cInputCls taskParamControlDataInput']";
-    private final String TASK_STATUS_SELECT = "//select[@class='Status__cInputCls taskParamControlDataInput']";
-    private final String CREATE_BUTTON = "//input[@class='ga-btnPrimary ga-savePlaybook' and @type='button' and @value='Create']";
-    private final String CANCEL_BUTTON = "//input[@class='ga-btnSecondary ga-cancelPlaybook' and @type='button' and @value ='Cancel']";
-    private final String UPDATE_BUTTON = "//input[@class='ga-btnPrimary ga-savePlaybook' and @value='Update']";
-    private final String ADD_TASK_BUTTON = "ga-addTaskIconId";
-    private final String TASK_SERCH_INPUT = "//input[@class='ga-searchInput ga-searchTasks'";
-    private final String TASK_SEARCH_CLEAR_BUTTON = "//input[@class='ga-clearBtn ga-searchTaskClearBtn']";
-    private final String PB_EDIT_LINK =  "//a[@class='ga-icn ga-edit ga-editPlaybook']";
-
-
-
 }
