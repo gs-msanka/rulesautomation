84db14fea5ee52cf5c36a228c819954bd3e266c6
Sri Vidya <srallapalli@gainsight.com>
2013-12-23 15:43:55 +0530

    [JB-3095] changes to support few more functions in the Milestones module

--- src/com/gainsight/sfdc/customer360/pages/Customer360Milestones.java
+++ src/com/gainsight/sfdc/customer360/pages/Customer360Milestones.java
@@ -1,5 +1,7 @@
 package com.gainsight.sfdc.customer360.pages;
 
+import org.openqa.selenium.By;
+
 public class Customer360Milestones extends Customer360Page {
 	
 	private final String MILESTONES_SUB_TAB="//li[@data-tabname='Milestones']/a[contains(.,'Milestones')]";
@@ -14,10 +16,17 @@ public class Customer360Milestones extends Customer360Page {
 	private final String SAVE_BUTTON="//a[@class='btn_save']";
 	private final String CLOSE_BUTTON="//a[@class='btn_cancel']";
 	private final String CLOSE_X="//button[@title='close']/span[@title='Close']";
-	private final String EDIT_LINK_ICON="//td[@class='edit-icon']";
-	private final String DELETE_LINK_ICON="//td[@class='delete-icon']";
 	private final String MILESTONES_TABLE="//table[@class='gs_features_grid gs_milestones_grid']";
+	private final String MILESTONES_TABLE_DATA_GRID=MILESTONES_TABLE+"/tbody";
+	private final String MILESTONE_DATE=MILESTONES_TABLE+"/tbody/tr[%d]/td[1]";
+	private final String MILESTONE_COLOR=MILESTONES_TABLE+"/tbody/tr[%d]/td[2]/span[@style='background-color:%s;']";
+	private final String MILESTONE_NAME=MILESTONES_TABLE+"/tbody/tr[%d]/td[2]/span[contains(.,'%s')]";
+	private final String MILESTONE_OPPOR=MILESTONES_TABLE+"/tbody/tr[%d]/td[3]";
+	private final String MILESTONE_COMMENTS=MILESTONES_TABLE+"/tbody/tr[%d]/td[4]";
+	private final String EDIT_LINK_ICON=MILESTONES_TABLE+"/tbody/tr[%d]/td[@class='edit-icon']";
+	private final String DELETE_LINK_ICON=MILESTONES_TABLE+"/tbody/tr[%d]/td[@class='delete-icon']";
 	private final String MILESSTONES_ADD_FRAME="//div[@class='ui-widget-overlay ui-front']";
+	private final String MILESTONE_ROW_CHECK=MILESTONES_TABLE+"/tbody/tr[%d]";
 
 	
 	public void gotoMilestonesSubtab(){
@@ -31,6 +40,22 @@ public class Customer360Milestones extends Customer360Page {
 		wait.waitTillElementDisplayed(MILESSTONES_ADD_FRAME, MIN_TIME, MAX_TIME);
 	}
 	
+	public boolean isHeaderPresent(){
+		wait.waitTillElementDisplayed(MILESTONES_TABLE, MIN_TIME, MAX_TIME);
+		return true;
+	}
+	
+	
+	public boolean isHeaderItemPresent(String columnName){
+		wait.waitTillElementDisplayed(MILESTONES_TABLE+ "/thead/tr/th[text()='"+ columnName+ "']",MIN_TIME,MAX_TIME);
+		return true;
+	}
+	
+	public boolean isMsTableDataPresent(){
+		System.out.println("xpath of table data-->"+MILESTONES_TABLE_DATA_GRID);
+		wait.waitTillElementDisplayed(MILESTONES_TABLE_DATA_GRID, MIN_TIME, MAX_TIME);
+		return true;
+	}
 	public void setDateInField(String date){
 		
 		/*JB-3102 needs to be fixed to select date from date-picker
@@ -59,6 +84,37 @@ public class Customer360Milestones extends Customer360Page {
 		item.clearAndSetText(COMMENT_FIELD, Comments);
 	}
 	
+	public boolean checkMilestoneDate(String date,int col){
+		amtDateUtil.stalePause();
+		System.out.println("got date-->"+date+", got date from UI-->"+item.getText(String.format(MILESTONE_DATE,col)));
+		return((item.getText(String.format(MILESTONE_DATE,col))).compareTo(date)==0 ? true : false);
+	}
+	
+	public boolean checkMilestoneColor(String color,int col){	
+		amtDateUtil.stalePause();
+		System.out.println("got color-->"+color+", got color from UI-->"+String.format(MILESTONE_COLOR,col,color));
+		wait.waitTillElementDisplayed(String.format(MILESTONE_COLOR,col,color), MIN_TIME, MAX_TIME);
+		return true;
+	}
+	
+	public boolean checkMilestoneName(String Name,int col){
+		amtDateUtil.stalePause();
+		System.out.println("got name-->"+Name+", got name from UI-->"+String.format(MILESTONE_NAME,col,Name));
+		wait.waitTillElementDisplayed(String.format(MILESTONE_NAME,col,Name), MIN_TIME, MAX_TIME);
+		return true;
+	}
+	
+	public boolean checkMilestoneOpportunity(String oppor,int col){
+		amtDateUtil.stalePause();
+		System.out.println("got opor-->"+oppor+", got op from UI-->"+item.getText(String.format(MILESTONE_OPPOR,col)));
+		return((item.getText(String.format(MILESTONE_OPPOR,col))).compareTo(oppor)==0 ? true : false);
+	}
+	
+	public boolean checkMilestoneComments(String comments,int col){
+		amtDateUtil.stalePause();
+		return ((item.getText(String.format(MILESTONE_COMMENTS,col))).compareTo(comments)==0 ? true : false);
+	}
+	
 	public void clickOnSave(){
 		item.click(SAVE_BUTTON);
 		wait.waitTillElementDisplayed(MILESTONES_TABLE, MIN_TIME, MAX_TIME);
@@ -74,14 +130,20 @@ public class Customer360Milestones extends Customer360Page {
 		wait.waitTillElementDisplayed(MILESTONES_TABLE, MIN_TIME, MAX_TIME);
 	}
 	
-	public void clickOnEditFeature(){
-		item.click(EDIT_LINK_ICON);
+	public void clickOnEditMilestone(int row){
+		item.click(String.format(EDIT_LINK_ICON,row));
 		wait.waitTillElementDisplayed(MILESSTONES_ADD_FRAME, MIN_TIME, MAX_TIME);
 	}
 	
-	public void clickOnDeleteFeature(){
-		item.click(DELETE_LINK_ICON);
+	public void clickOnDeleteMilestone(int row){
+		item.click(String.format(DELETE_LINK_ICON,row));
 		driver.switchTo().alert().accept();
 		wait.waitTillElementDisplayed(MILESTONES_TABLE, MIN_TIME, MAX_TIME);
+	}
+	
+	public boolean isRowPresentAfterDelete(int row)
+	{
+		wait.waitTillElementNotPresent(String.format(MILESTONE_ROW_CHECK, row), MIN_TIME, MAX_TIME);
+		return true;
 	}
 }
