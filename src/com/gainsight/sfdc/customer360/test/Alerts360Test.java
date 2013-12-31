467314eae36f9cfe33f0afcf4a5ea7a57180b25f
gs-golla <ggolla@gainsight.com>
2013-12-31 18:35:23 +0530

    Alerts 360 Test cases & Page objects Updated.

--- src/com/gainsight/sfdc/customer360/test/Alerts360Test.java
+++ src/com/gainsight/sfdc/customer360/test/Alerts360Test.java
@@ -34,19 +34,70 @@ public class Alerts360Test extends BaseTest {
     @DataProviderArguments(filePath = TEST_DATA_FILE, sheet = "360_Alert_1")
     public void addAlert(HashMap<String , String> testData) {
         HashMap<String, String> alertData = getMapFromData(testData.get("Alert"));
-        AlertCardLabel alertCardLabel = new AlertCardLabel();
-        alertCardLabel.setLabel5("Alert ASV");
-        alertCardLabel.setLabel4("Reason");
-        alertCardLabel.setLabel3("Type");
-        alertCardLabel.setLabel2("Severity");
-        alertCardLabel.setLabel1("Status");
+        AlertCardLabel alabel = new AlertCardLabel();
+        alabel.setLabel5("Alert ASV");
+        alabel.setLabel4("Reason");
+        alabel.setLabel3("Type");
+        alabel.setLabel2("Severity");
+        alabel.setLabel1("Status");
         alertData.put("date", getDatewithFormat(0));
-        if(testData.get("Alert") != null) {
-            ret.addAlert(alertData);
-        } else {
-            Assert.assertTrue(false, "Please check your data set");
-        }
-     ret.isAlertDisplayed(alertData, alertCardLabel);
+        ret.addAlert(alertData);
+        alertData.put(alabel.getLabel1(), alertData.get("status"));
+        alertData.put(alabel.getLabel2(), alertData.get("severity"));
+        alertData.put(alabel.getLabel3(), alertData.get("type"));
+        alertData.put(alabel.getLabel4(), alertData.get("reason"));
+        alertData.put(alabel.getLabel5(), alertData.get("asv"));
+        ret.isAlertDisplayed(alertData, alabel);
+    }
+
+    @Test(dataProviderClass = com.gainsight.utils.ExcelDataProvider.class, dataProvider = "excel")
+    @DataProviderArguments(filePath = TEST_DATA_FILE, sheet = "360_Alert_2")
+    public void deleteAlert(HashMap<String , String> testData) {
+        HashMap<String, String> alertData = getMapFromData(testData.get("Alert"));
+        AlertCardLabel alabel = new AlertCardLabel();
+        alabel.setLabel5("Alert ASV");
+        alabel.setLabel4("Reason");
+        alabel.setLabel3("Type");
+        alabel.setLabel2("Severity");
+        alabel.setLabel1("Status");
+        alertData.put("date", getDatewithFormat(0));
+        ret.addAlert(alertData);
+        alertData.put(alabel.getLabel1(), alertData.get("status"));
+        alertData.put(alabel.getLabel2(), alertData.get("severity"));
+        alertData.put(alabel.getLabel3(), alertData.get("type"));
+        alertData.put(alabel.getLabel4(), alertData.get("reason"));
+        alertData.put(alabel.getLabel5(), alertData.get("asv"));
+        Assert.assertEquals(true, ret.isAlertDisplayed(alertData,alabel), "Checking weather alert created successfully");
+        ret.deleteAlert(alertData, alabel);
+        Assert.assertEquals(false, ret.isAlertDisplayed(alertData,alabel), "Checking weather alert is deleted successfully");
+    }
+
+    @Test(dataProviderClass = com.gainsight.utils.ExcelDataProvider.class, dataProvider = "excel")
+    @DataProviderArguments(filePath = TEST_DATA_FILE, sheet = "360_Alert_3")
+    public void updateAlerts(HashMap<String , String> testData) {
+        HashMap<String, String> alertData = getMapFromData(testData.get("Alert"));
+        HashMap<String, String> updatedAlertData = getMapFromData(testData.get("Updated_Alert"));
+        AlertCardLabel alabel = new AlertCardLabel();
+        alabel.setLabel5("Alert ASV");
+        alabel.setLabel4("Reason");
+        alabel.setLabel3("Type");
+        alabel.setLabel2("Severity");
+        alabel.setLabel1("Status");
+        alertData.put("date", getDatewithFormat(0));
+        ret.addAlert(alertData);
+        alertData.put(alabel.getLabel1(), alertData.get("status"));
+        alertData.put(alabel.getLabel2(), alertData.get("severity"));
+        alertData.put(alabel.getLabel3(), alertData.get("type"));
+        alertData.put(alabel.getLabel4(), alertData.get("reason"));
+        alertData.put(alabel.getLabel5(), alertData.get("asv"));
+        Assert.assertEquals(true, ret.isAlertDisplayed(alertData,alabel), "Checking weather alert created successfully");
+        ret.updateAlert(alertData, updatedAlertData, alabel);
+        updatedAlertData.put(alabel.getLabel1(), alertData.get("status"));
+        updatedAlertData.put(alabel.getLabel2(), alertData.get("severity"));
+        updatedAlertData.put(alabel.getLabel3(), alertData.get("type"));
+        updatedAlertData.put(alabel.getLabel4(), alertData.get("reason"));
+        updatedAlertData.put(alabel.getLabel5(), alertData.get("asv"));
+        Assert.assertEquals(true, ret.isAlertDisplayed(updatedAlertData,alabel), "Checking weather alert is updated successfully");
     }
 
 
