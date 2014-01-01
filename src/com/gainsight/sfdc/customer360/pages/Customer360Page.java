384a17a6d0ab1ca2173851f8a7170cf7f94664bb
Sri Vidya <srallapalli@gainsight.com>
2014-01-01 16:30:29 +0530

    [JB-3168] Support for Scorecard tests

--- src/com/gainsight/sfdc/customer360/pages/Customer360Page.java
+++ src/com/gainsight/sfdc/customer360/pages/Customer360Page.java
@@ -81,6 +81,9 @@ public class Customer360Page extends BasePage {
 		if(name.equals("Usage")){
 			return new Customer360Milestones();
 		}
+		if(name.equals("Scorecard")){
+			return new Customer360Scorecard();
+		}
 		return this;
 	}
 
