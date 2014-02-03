aaff7d3cbcb306929511a99653158465f80f33ec
Sunand P <spadmanabhan@gainsight.com>
2014-02-03 19:12:37 +0530

    Some More files.

---
+++ src/com/gainsight/sfdc/util/bulk/SfdcOperationType.java
@@ -0,0 +1,12 @@
+package com.gainsight.sfdc.util.bulk;
+
+public enum SfdcOperationType {
+
+	/**
+	 * SFDC Bulk Operation Types
+	 */
+	query,
+	insert,
+	upsert,
+	delete
+}
---
+++ src/com/gainsight/sfdc/util/datagen/JobType.java
@@ -0,0 +1,7 @@
+package com.gainsight.sfdc.util.datagen;
+
+public enum JobType {
+	UsageData,
+	Transaction,
+	Survey
+}
