a56258e709ebd50b7331a0d6cac9557c8a0a5c91
gs-spulluri <spulluri@gainsight.com>
2014-02-21 12:30:20 +0530

    Adding Scripts for Customer_Tab_ UI_Views
    Signed-off-by: gs-spulluri <spulluri@gainsight.com>

--- src/com/gainsight/sfdc/administration/tests/AdminUIViewsTabTest.java
+++ src/com/gainsight/sfdc/administration/tests/AdminUIViewsTabTest.java
@@ -22,13 +22,13 @@ public class AdminUIViewsTabTest extends BaseTest {
 	//private final String TESTDATA_DIR = TEST_DATA_PATH_PREFIX
 		//	+ generatePath(dirs);
 	final String TEST_DATA_FILE = "testdata/sfdc/Administration/AdminUIViewsTestdata.xls";
+	
 	@BeforeClass
 	public void setUp() {
 		Report.logInfo("Starting  Test Case...");
-		
 		apex.runApexCodeFromFile(env.basedir+"/apex_scripts/UI_Views/AlertUI-Views", isPackageInstance());
+		apex.runApexCodeFromFile(env.basedir+"/apex_scripts/UI_Views/Customer_Tab_ UI_Views", isPackageInstance());
 		basepage.login();
-		
 	}
 	
 	@Test(dataProviderClass = com.gainsight.utils.ExcelDataProvider.class, dataProvider = "excel",priority=1)
@@ -40,16 +40,16 @@ public class AdminUIViewsTabTest extends BaseTest {
 		HashMap<String, String> data = getMapFromData1(testData);
 		String tabName = data.get("tabName");
 		String ViewName =data.get("ViewName");
-		String fieldName = data.get("fieldName");
+		String sFieldName = data.get("sFieldName");
 		String selectffield = data.get("selectffield");
 		System.out.println("selectffield has these values in test:" +selectffield);
 		String foperator =data.get("foperator");
-		String selectfvalue = data.get("selectfvalue");
+		String fvalue = data.get("fvalue");
 		AdminUIViewssSubTab adUIview = basepage.clickOnAdminTab().clickOnUIViewssettingsSubTab();
-		adUIview.selectTabName(tabName,ViewName,fieldName,selectffield,foperator,selectfvalue); 
+		adUIview.selectTabName(tabName,ViewName,sFieldName,selectffield,foperator,fvalue); 
 		return adUIview;
 	}
-	
+//	selectfvalue
 	public HashMap<String, String> getMapFromData1(String data) {
 		HashMap<String, String> hm = new HashMap<String, String>();
 		System.out.println(data);
