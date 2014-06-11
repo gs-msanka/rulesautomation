3efdf409de60237e8b0f8eccf47ad05a5cd04905
gs-golla <ggolla@gainsight.com>
2014-06-11 19:30:29 +0530

    Adoption base page updated to New UI.

--- src/com/gainsight/sfdc/adoption/pages/AdoptionBasePage.java
+++ src/com/gainsight/sfdc/adoption/pages/AdoptionBasePage.java
@@ -4,16 +4,21 @@ package com.gainsight.sfdc.adoption.pages;
 import com.gainsight.sfdc.pages.BasePage;
 
 public class AdoptionBasePage extends BasePage {
-    private final String READY_INDICATOR="//a[text()='Analytics']";
+
+    private final String READY_INDICATOR = "//a[@data-tab='ADOPTION']";
+    private final String OVER_SUB_TAB = "//a[@data-tab='ADOPTION']";
+    private final String TRENDS_SUB_TAB = "//a[@data-tab='TRENDS']";
+    private final String EXPLORER_SUB_TAB = "//a[@data-tab='ANALYTICS']";
 
     public AdoptionBasePage() {
         wait.waitTillElementPresent(READY_INDICATOR, MIN_TIME, MAX_TIME);
     }
+
     /**
      * Click on Adoption Grid Page
-     * @return AdoptinosUsagePage
+     * @return AdoptionUsagePage
      */
-    public AdoptionUsagePage clickOnUsageGridSubTab(){
+    public AdoptionUsagePage clickOnUsageGridSubTab() {
         item.click("//a[text()='Usage']");
         return new AdoptionUsagePage();
     }
@@ -22,8 +27,25 @@ public class AdoptionBasePage extends BasePage {
      * Clicks on adoption analytics page.
      * @return adoption analytics page.
      */
-    public AdoptionAnalyticsPage clickOnUsageAnalyticsTab(){
+    public AdoptionAnalyticsPage clickOnUsageAnalyticsTab() {
         item.click("//a[text()='Analytics']");
         return new AdoptionAnalyticsPage();
     }
+
+
+    public AdoptionUsagePage clickOnOverviewSubTab()  {
+        item.click(OVER_SUB_TAB);
+        return new AdoptionUsagePage();
+    }
+
+    public AdoptionAnalyticsPage clickOnTrendsSubTab() {
+        item.click(TRENDS_SUB_TAB);
+        return new AdoptionAnalyticsPage();
+    }
+
+    public AdoptionExplorerPage clickOnExplorerSubTab() {
+        item.click(EXPLORER_SUB_TAB);
+        return new AdoptionExplorerPage();
+    }
+
 }
