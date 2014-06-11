6737a671668052a3a8d1fb620984ae19908019db
gs-golla <ggolla@gainsight.com>
2014-06-11 19:32:41 +0530

    Added explorer page.

---
+++ src/com/gainsight/sfdc/adoption/pages/AdoptionExplorerPage.java
@@ -0,0 +1,11 @@
+package com.gainsight.sfdc.adoption.pages;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gainsight
+ * Date: 11/06/14
+ * Time: 11:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AdoptionExplorerPage {
+}
