9fd48e76cf6f1a68e4c08da3007037ac17c548e0
gs-golla <ggolla@gainsight.com>
2014-06-19 12:25:53 +0530

    Adding 5 more fields to usage data object

--- src/com/gainsight/sfdc/tests/BaseTest.java
+++ src/com/gainsight/sfdc/tests/BaseTest.java
@@ -12,197 +12,208 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 import java.io.*;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class BaseTest {
-	protected TestDataHolder testDataLoader = new TestDataHolder();
-	String[] dirs = { "testdata", "sfdc" };
-	protected TestEnvironment env = new TestEnvironment();
-	public final String TEST_DATA_PATH_PREFIX = env.basedir + "/"
-			+ generatePath(dirs);
-	public SOQLUtil soql = new SOQLUtil();
-	public ApexUtil apex = new ApexUtil();
-	protected static BasePage basepage;
+    protected TestDataHolder testDataLoader = new TestDataHolder();
+    String[] dirs = {"testdata", "sfdc"};
+    protected TestEnvironment env = new TestEnvironment();
+    public final String TEST_DATA_PATH_PREFIX = env.basedir + "/"
+            + generatePath(dirs);
+    public SOQLUtil soql = new SOQLUtil();
+    public ApexUtil apex = new ApexUtil();
+    protected static BasePage basepage;
     public static String userLocale;
     public static TimeZone userTimezone;
     public String userDir = env.basedir;
-    public AmountsAndDatesUtil adUtil=new AmountsAndDatesUtil();
-	
+    public AmountsAndDatesUtil adUtil = new AmountsAndDatesUtil();
+    public HashMap<String, String> monthMap = new HashMap<String, String>();
+    Calendar c = Calendar.getInstance();
 
-	@BeforeSuite
-	public void init() throws Exception {
-		Report.logInfo("Initializing Environment");
-		env.start();
-		try {
-			String setAsDefaultApp = env.getProperty("sfdc.setAsDefaultApp");
-			String loadDefaultData = env.getProperty("sfdc.loadDefaultData");
-			env.launchBrower();
-			basepage = new BasePage();
+
+    @BeforeSuite
+    public void init() throws Exception {
+        Report.logInfo("Initializing Environment");
+        env.start();
+        monthMap.put("1", "Jan");
+        monthMap.put("2", "Feb");
+        monthMap.put("3", "Mar");
+        monthMap.put("4", "Apr");
+        monthMap.put("5", "May");
+        monthMap.put("6", "Jun");
+        monthMap.put("7", "Jul");
+        monthMap.put("8", "Aug");
+        monthMap.put("9", "Sep");
+        monthMap.put("10", "Oct");
+        monthMap.put("11", "Nov");
+        monthMap.put("12", "Dec");
+        try {
+            String setAsDefaultApp = env.getProperty("sfdc.setAsDefaultApp");
+            String loadDefaultData = env.getProperty("sfdc.loadDefaultData");
+            env.launchBrower();
+            basepage = new BasePage();
             userLocale = soql.getUserLocale();
-            userTimezone=TimeZone.getTimeZone(soql.getUserTimeZone());
-			Report.logInfo("Initializing Base Page : " + basepage);
-			if ((setAsDefaultApp != null && setAsDefaultApp.equals("true")) || loadDefaultData != null && loadDefaultData.equals("true")) {
-				basepage.login();
-                if((setAsDefaultApp != null && setAsDefaultApp.equals("true"))) {
+            userTimezone = TimeZone.getTimeZone(soql.getUserTimeZone());
+            Report.logInfo("Initializing Base Page : " + basepage);
+            if ((setAsDefaultApp != null && setAsDefaultApp.equals("true")) || loadDefaultData != null && loadDefaultData.equals("true")) {
+                basepage.login();
+                if ((setAsDefaultApp != null && setAsDefaultApp.equals("true"))) {
                     basepage.setDefaultApplication("Gainsight");
                     basepage.addTabsToApplication("Gainsight", "Customer Success 360, Gainsight");
                 }
-                if(loadDefaultData != null && loadDefaultData.equals("true")) {
+                if (loadDefaultData != null && loadDefaultData.equals("true")) {
                     basepage.loadDefaultData();
                 }
                 basepage.logout();
-			}
+            }
 
-		} catch (Exception e) {
-			env.stop();
-			Report.logInfo(e.getLocalizedMessage());
-			e.printStackTrace();
-			throw e;
-		}
-	}
+        } catch (Exception e) {
+            env.stop();
+            Report.logInfo(e.getLocalizedMessage());
+            e.printStackTrace();
+            throw e;
+        }
+    }
 
-	@AfterSuite
-	public void fini() {
-		env.stop();
-	}
+    @AfterSuite
+    public void fini() {
+        env.stop();
+    }
 
-	@BeforeClass
-	public void failureRecovery() {
-		if (env.getDriver() == null) {
-			env.start();
-		}
-	}
+    @BeforeClass
+    public void failureRecovery() {
+        if (env.getDriver() == null) {
+            env.start();
+        }
+    }
 
-	@BeforeMethod
-	public void beInMainWindow() {
-		basepage.beInMainWindow();
-	}
+    @BeforeMethod
+    public void beInMainWindow() {
+        basepage.beInMainWindow();
+    }
 
-	public String generatePath(String[] dirs) {
-		String path = "";
-		for (String dir : dirs) {
-			path = path + dir + File.separator;
-		}
-		return path;
-	}
+    public String generatePath(String[] dirs) {
+        String path = "";
+        for (String dir : dirs) {
+            path = path + dir + File.separator;
+        }
+        return path;
+    }
 
-	public String currencyFormat(String amt) {
-		DecimalFormat moneyFormat = new DecimalFormat("$###,###");
-		return moneyFormat.format(new Long(amt)).replace("$", "$ ");
-	}
+    public String currencyFormat(String amt) {
+        DecimalFormat moneyFormat = new DecimalFormat("$###,###");
+        return moneyFormat.format(new Long(amt)).replace("$", "$ ");
+    }
 
-	
 
-	public HashMap<String, String> getMapFromData(String data) {
-		HashMap<String, String> hm = new HashMap<String, String>();
-		System.out.println(data);
-		String[] dataArray = data.substring(1, data.length() - 1).split("\\|");
-		for (String record : dataArray) {
-			if (record != null) {
-				System.out.println(record);
-				String[] pair = record.split("\\:");
-				hm.put(pair[0], pair[1].trim());
-			}
-		}
-		return hm;
-	}
+    public HashMap<String, String> getMapFromData(String data) {
+        HashMap<String, String> hm = new HashMap<String, String>();
+        System.out.println(data);
+        String[] dataArray = data.substring(1, data.length() - 1).split("\\|");
+        for (String record : dataArray) {
+            if (record != null) {
+                System.out.println(record);
+                String[] pair = record.split("\\:");
+                hm.put(pair[0], pair[1].trim());
+            }
+        }
+        return hm;
+    }
 
-	public List<HashMap<String, String>> getMapFromDataList(String data) {
-		List<HashMap<String, String>> hm = new ArrayList();
-		System.out.println(data);
-		String[] dataArray = data.substring(1, data.length() - 1).split(",");
-		int i = 0;
-		for (String record : dataArray) {
-			if (record != null) {
-				System.out.println(record);
-				hm.add(getMapFromData("{"+record+"}"));
-				i++;
-			}
-		}
-		return hm;
-	}
+    public List<HashMap<String, String>> getMapFromDataList(String data) {
+        List<HashMap<String, String>> hm = new ArrayList();
+        System.out.println(data);
+        String[] dataArray = data.substring(1, data.length() - 1).split(",");
+        int i = 0;
+        for (String record : dataArray) {
+            if (record != null) {
+                System.out.println(record);
+                hm.add(getMapFromData("{" + record + "}"));
+                i++;
+            }
+        }
+        return hm;
+    }
 
-	public int calcMRR(int ASV) {
-		return (int) Math.round(ASV / 12.0);
-	}
+    public int calcMRR(int ASV) {
+        return (int) Math.round(ASV / 12.0);
+    }
 
-	public int calcARPU(int ASV, int users) {
-		return (ASV / 12) / users;
-	}
+    public int calcARPU(int ASV, int users) {
+        return (ASV / 12) / users;
+    }
 
-	public String makeRowValues(String... values) {
-		String row = "";
-		int counter = 1;
-		int size = values.length;
-		for (String value : values) {
-			if (counter == size) {
-				row = row + value;
-			} else {
-				row = row + value + "|";
-			}
-			counter++;
-		}
-		return row;
-	}
+    public String makeRowValues(String... values) {
+        String row = "";
+        int counter = 1;
+        int size = values.length;
+        for (String value : values) {
+            if (counter == size) {
+                row = row + value;
+            } else {
+                row = row + value + "|";
+            }
+            counter++;
+        }
+        return row;
+    }
 
-	/**
-	 * @return true if the execution context is packaged environment.
-	 */
-	public boolean isPackageInstance() {
-		Boolean namespace = Boolean.valueOf(env
-				.getProperty("sfdc.managedPackage"));
-		System.out.println("Is Managed Package :" + namespace);
-		return namespace;
-	}
+    /**
+     * @return true if the execution context is packaged environment.
+     */
+    public boolean isPackageInstance() {
+        Boolean namespace = Boolean.valueOf(env
+                .getProperty("sfdc.managedPackage"));
+        System.out.println("Is Managed Package :" + namespace);
+        return namespace;
+    }
 
-	/**
-	 * This Method queries the data base with the query specified.
-	 * 
-	 * @param query
-	 *            - The Query to Execute on salesforce.
-	 * @return integer - Count of records that the query returned.
-	 */
-	public int getQueryRecordCount(String query) {
-		int result = 0;
+    /**
+     * This Method queries the data base with the query specified.
+     *
+     * @param query - The Query to Execute on salesforce.
+     * @return integer - Count of records that the query returned.
+     */
+    public int getQueryRecordCount(String query) {
+        int result = 0;
         result = soql.getRecordCount(resolveStrNameSpace(query));
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * Method to remove the name space from the string "JBCXM__".
-	 * 
-	 * @param str
-	 *            -The string where name space should be removed.
-	 * @return String - with name space removed.
-	 */
-	public String resolveStrNameSpace(String str) {
-		String result = "";
+    /**
+     * Method to remove the name space from the string "JBCXM__".
+     *
+     * @param str -The string where name space should be removed.
+     * @return String - with name space removed.
+     */
+    public String resolveStrNameSpace(String str) {
+        String result = "";
         boolean isPackage = Boolean.valueOf(env.getProperty("sfdc.managedPackage"));
-		if (str != null && !isPackage) {
-            result =  str.replaceAll("JBCXM__", "").replaceAll("JBCXM\\.", "");
+        if (str != null && !isPackage) {
+            result = str.replaceAll("JBCXM__", "").replaceAll("JBCXM\\.", "");
             Report.logInfo(result);
             return result;
-		} else {
+        } else {
             return str;
         }
 
-	}
+    }
 
     public String getDatewithFormat(int i) {
-        String date                 = null;
+        String date = null;
         Calendar c = Calendar.getInstance();
         c.add(Calendar.DATE, i);
-        if(userLocale.contains("en_US")) {
-            DateFormat dateFormat   = new SimpleDateFormat("M/d/yyyy");
+        if (userLocale.contains("en_US")) {
+            DateFormat dateFormat = new SimpleDateFormat("M/d/yyyy");
             date = dateFormat.format(c.getTime());
 
-        } else if(userLocale.contains("en_IN")) {
-            DateFormat dateFormat   = new SimpleDateFormat("d/M/yyyy");
+        } else if (userLocale.contains("en_IN")) {
+            DateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
             date = dateFormat.format(c.getTime());
         }
         Report.logInfo(String.valueOf(date));
@@ -210,24 +221,24 @@ public class BaseTest {
     }
 
     public String getDateFormat(int i) {
-        String date                 = null;
+        String date = null;
         Calendar c = Calendar.getInstance();
         c.add(Calendar.DATE, i);
-        if(userLocale.contains("en_US")) {
-            DateFormat dateFormat   = new SimpleDateFormat("MM/dd/yyyy");
+        if (userLocale.contains("en_US")) {
+            DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
             date = dateFormat.format(c.getTime());
 
-        } else if(userLocale.contains("en_IN")) {
-            DateFormat dateFormat   = new SimpleDateFormat("dd/MM/yyyy");
+        } else if (userLocale.contains("en_IN")) {
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
             date = dateFormat.format(c.getTime());
         }
         Report.logInfo(String.valueOf(date));
         return date;
     }
-    
+
     public void deletePickList() {
-    	String DELETE_SCRIPT_FILE = env.basedir+"/testdata/sfdc/Administration/Picklist_Delte_Script.txt";
-    	apex.runApexCodeFromFile(DELETE_SCRIPT_FILE, isPackageInstance());
+        String DELETE_SCRIPT_FILE = env.basedir + "/testdata/sfdc/Administration/Picklist_Delte_Script.txt";
+        apex.runApexCodeFromFile(DELETE_SCRIPT_FILE, isPackageInstance());
     }
 
     public String getFileContents(String fileName) {
@@ -249,8 +260,8 @@ public class BaseTest {
     }
 
     public FileReader resolveNameSpace(String fileName) throws FileNotFoundException {
-        if(!isPackageInstance()) {
-            File tempFile = new File(env.basedir+"/resources/datagen/process/tempJob.txt");
+        if (!isPackageInstance()) {
+            File tempFile = new File(env.basedir + "/resources/datagen/process/tempJob.txt");
             FileOutputStream fOut = new FileOutputStream(tempFile);
             try {
                 fOut.write(resolveStrNameSpace(getFileContents(fileName)).getBytes());
@@ -259,7 +270,7 @@ public class BaseTest {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return new FileReader(env.basedir+"/resources/datagen/process/tempJob.txt");
+            return new FileReader(env.basedir + "/resources/datagen/process/tempJob.txt");
         } else {
             return new FileReader(fileName);
 
@@ -271,9 +282,9 @@ public class BaseTest {
     public void createExtIdFieldOnAccount() {
         CreateObjectAndFields fieldsCreator = new CreateObjectAndFields();
         try {
-            fieldsCreator.createTextFields("Account", new String[]{"Data ExternalId"}, true, true,true,false,false);
+            fieldsCreator.createTextFields("Account", new String[]{"Data ExternalId"}, true, true, true, false, false);
         } catch (Exception e) {
-            Report.logInfo("Failed to create ext id field on account object :" +e.getLocalizedMessage());
+            Report.logInfo("Failed to create ext id field on account object :" + e.getLocalizedMessage());
             e.printStackTrace();
         }
 
@@ -281,27 +292,30 @@ public class BaseTest {
 
     public void createFieldsOnUsageData() {
         String object = "JBCXM__Usagedata__c";
-        String[] numberFields = new String[]{"Page Views", "Page Visits", "No of Report Run", "Files Downloaded"};
+        String[] numberFields1 = new String[]{"Page Views", "Page Visits", "No of Report Run", "Files Downloaded"};
+        String[] numberFields2 = new String[]{"Emails Sent Count", "Leads", "No of Campaigns", "DB Size", "Active Users"};
         CreateObjectAndFields cObjFields = new CreateObjectAndFields();
         try {
-            cObjFields.createNumberField(resolveStrNameSpace(object), numberFields, false);
+            cObjFields.createNumberField(resolveStrNameSpace(object), numberFields1, false);
+            cObjFields.createNumberField(resolveStrNameSpace(object), numberFields2, false);
         } catch (Exception e) {
-            Report.logInfo("Failed to create Fields on the object :" +object);
+            Report.logInfo("Failed to create Fields on the object :" + object);
             e.printStackTrace();
         }
     }
 
     /**
      * This parameter returns the String with comprises of yyyy|mm|dd format.
-     * @param weekDay - Expected values Sun, Mon, Tue, Wed, Thu, Fri, Sat.
+     *
+     * @param weekDay   - Expected values Sun, Mon, Tue, Wed, Thu, Fri, Sat.
      * @param daysToAdd - number of days to add for current day.
      * @return String of format "yyyy|mm|dd".
      */
     public String getWeekLabelDate(String weekDay, int daysToAdd, boolean usesEndDate, boolean userFormat) {
-        String date= null;
+        String date = null;
         try {
             Calendar c = Calendar.getInstance();
-            Map<String,Integer> days = new HashMap<String, Integer>();
+            Map<String, Integer> days = new HashMap<String, Integer>();
             days.put("Sun", 1);
             days.put("Mon", 2);
             days.put("Tue", 3);
@@ -310,8 +324,8 @@ public class BaseTest {
             days.put("Fri", 6);
             days.put("Sat", 7);
             int weekDate = days.get(weekDay);
-            if(usesEndDate) {
-                weekDate = (weekDate ==1) ? 7 : weekDate-1;
+            if (usesEndDate) {
+                weekDate = (weekDate == 1) ? 7 : weekDate - 1;
             }
             c.set(Calendar.DAY_OF_WEEK, weekDate);
             System.out.println(c.get(Calendar.DATE));
@@ -320,21 +334,47 @@ public class BaseTest {
             int month = c.get(Calendar.MONTH);
             month += 1;
             int year = c.get(Calendar.YEAR);
-            if(userFormat) {
-                if(userLocale.contains("en_US")) {
-                    date = month + "/" +day + "/"+year;
+            if (userFormat) {
+                if (userLocale.contains("en_US")) {
+                    date = month + "/" + day + "/" + year;
 
-                } else if(userLocale.contains("en_IN")) {
-                    date = day + "/" +month + "/" +year;
+                } else if (userLocale.contains("en_IN")) {
+                    date = day + "/" + month + "/" + year;
                 }
             } else {
-                date = year + "|" + month + "|"+day;
+                date = year + "|" + month + "|" + day;
             }
 
         } catch (NullPointerException e) {
             Report.logInfo(e.getLocalizedMessage());
         }
+        Report.logInfo("Week Label Date is : " +date);
         return date;
+    }
+
+    public String[] setMonthAndYear(int numOfMonthsToAdd) {
+        int month, year;
+        int currentMonth = Integer.valueOf(c.get(Calendar.MONTH));
+        int currentYear = Integer.valueOf(c.get(Calendar.YEAR));
+        if (numOfMonthsToAdd == 0) {
+            if (currentMonth == 0) {
+                month = 12;
+                year = currentYear - 1;
+            } else {
+                month = currentMonth;
+                year = currentYear;
+            }
+        } else {
+            if ((currentMonth + numOfMonthsToAdd) - 11 > 0) {
+                month = currentMonth + numOfMonthsToAdd - 11;
+                year = currentYear + 1;
+            } else {
+                month = currentMonth + numOfMonthsToAdd;
+                year = currentYear;
+            }
+        }
+        Report.logInfo("Month : " +month + " - Year : " +year);
+        return new String[]{String.valueOf(month), String.valueOf(year)};
     }
 
 
