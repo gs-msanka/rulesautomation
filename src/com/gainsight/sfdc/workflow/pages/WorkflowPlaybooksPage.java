1d8af3624495384a06135be0dba5daa2567e9919
gs-jvunnam <jvunnam@gainsight.com>
2014-08-06 10:21:20 +0530

    Added Xpaths for Workflow Playbook elements

--- src/com/gainsight/sfdc/workflow/pages/WorkflowPlaybooksPage.java
+++ src/com/gainsight/sfdc/workflow/pages/WorkflowPlaybooksPage.java
@@ -14,20 +14,48 @@ public class WorkflowPlaybooksPage {
 	private final String OPPORTUNITY_PBTYPE_RADIO 	= "//input[@type='radio' and @value='Opportunity' and @name='playbook-type']";	//Opportunity Radio button
 	private final String EVENT_PBTYPE_RADIO 		= "//input[@type='radio' and @value='Event' and @name='playbook-type']";	//Event Radio button
 	private final String ALL_PBTYPE_RADIO 			= "//input[@type='radio' and @value='All' and @name='playbook-type']";	//All Radio button
+	private final String DUPLICATE_PLAYBOOK			= "//a[@title='Duplicate playbook']";	//Duplicate Playbook Icon
+	private final String EDIT_PLAYBOOK				= "//a[@title='Edit playbook']";	//Edit Playbook
+	private final String DELETE_PLAYBOOK			= "//a[@title='Delete playbook']";	//Delete Playbook
+	private final String SEARCH_PLAYBOOK_INPUT		= "//input[@class='form-control global-search']";	//Search Playbook 
+	private final String SEARCH_TASK_INPUT			= "//input[@class='form-control global-search col-md-12 search-playbooks-tasks']";	//Search Task
+	private final String ADD_PBTASK_BUTTON			= "//input[@class='gs-btn btn-add  btn-add-task']";	//Add Task
+	private final String EDIT_PBTASK_BUTTON			= "//a[@title='Edit task']";		//Edit Task
+	private final String DELETE_PBTASK_BUTTON		= "//a[@title='Delete task']";	//Delete Task
+	private final String SUBJECT_PBTASK_INPUT		= "//input[@class='form-control Subject__cInputCls taskParamControlDataInput']";		//Task Subject
+	private final String RELDATECOUNT_PBTASK_INPUT	= "//input[@class='form-control width40 Date__cInputCls taskParamControlDataInput']";	//Task Date
+	private final String COMMENTS_PBTASK_INPUT		= "//textarea[@class='form-control Description__cInputCls taskParamControlDataInput']";	//Task Comments/Description
+	private final String STATUS_PBTASK_BUTTON 		="//label[@class='Status__cClass taskFieldLbl ga-flabel col-sm-2 control-label']/parent::div/div/button/span";		//Task Status
+	private final String PRIORITY_PBTASK_BUTTON		="//label[@class='Priority__cClass taskFieldLbl ga-flabel col-sm-2 control-label']/parent::div/div/button/span";	//Task Priority	
 	
-	private final String DUPLICATE_PLAYBOOK			="//a[@title='Duplicate playbook']";	//Duplicate Playbook Icon
-	private final String EDIT_PLAYBOOK				="//a[@title='Edit playbook']";	//Edit Playbook
-	private final String DELETE_PLAYBOOK			="//a[@title='Delete playbook']";	//Delete Playbook
+	/**
+     * Constructor of the page, Waits for the ready indicator to be present on the page.
+     */
+    public PlayBooksPage() {
+        wait.waitTillElementDisplayed(READY_INDICATOR, MIN_TIME, MAX_TIME);
+    }
+    
+    /**
+     * Adds Playbook for the user on supplied paramters.  
+     * <pre>
+     * {@code
+     * 	1. Clicks on "Add Playbook" button.
+     * 	2. Fills the Playbook details.
+     * 	3. Fills the Event details.	
+     * 	4. Clicks on create button.
+     * }
+     * @param playbookData - a HashMap where all the data needed a Playbook exists.
+     * @param taskData - a HashMap where  all the data needed a Task exists.
+     * @return void
+     * </pre>
+     */
+    public void addplaybook(HashMap<String, String> playbookData, HashMap<String, String> taskData) {
+		/*item.click(ADD_PLAYBOOK_BUTTON);
+        wait.waitTillElementDisplayed(CREATE_BUTTON, MIN_TIME, MAX_TIME);
+        fillPlaybookDetails(playbookData);
+        fillTaskDetails(taskData);
+        item.click(CREATE_BUTTON);
+        amtDateUtil.stalePause();*/
+    }
 	
-	private final String SEARCH_PLAYBOOK_INPUT		="//input[@class='form-control global-search']";	//Search Playbook 
-	private final String SEARCH_TASK_INPUT			="//input[@class='form-control global-search col-md-12 search-playbooks-tasks']";	//Search Task
-	
-	private final String ADD_PBTASK_BUTTON			="//input[@class='gs-btn btn-add  btn-add-task']";	//Add Task
-	//Edit Task
-	//Delete Task
-	//Task Subject
-	//Task Priority
-	//Task Date
-	//Task Status
-	//Task Description
 }
