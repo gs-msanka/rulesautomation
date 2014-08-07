340f07af5d247fd9160adb10725311a81352924d
gs-golla <ggolla@gainsight.com>
2014-08-07 16:25:37 +0530

    Updated 360-Milestone test cases, included in XML.

--- src/com/gainsight/sfdc/customer360/pages/Customer360Milestones.java
+++ src/com/gainsight/sfdc/customer360/pages/Customer360Milestones.java
@@ -2,29 +2,30 @@ package com.gainsight.sfdc.customer360.pages;
 
 public class Customer360Milestones extends Customer360Page {
 	
-	private final String NO_MILESTONES_MSG = "//div[@class='noMilestone noDataFound' and contains(.,'No Milestones Found')]";
-	private final String MILESTONES_SUB_TAB="//li[@data-tabname='Milestones']/a[contains(.,'Milestones')]";
-	private final String ADD_MILESTONES="//a[@class='addNewMilestone']";
-	private final String DATE_FIELD="//input[@id='DateId']";
-	private final String MILESTONE_DROP_BOX="//select[@id='MilestoneOptions']";
-	private final String OPPORTUNITY_DROP_BOX="//select[@id='OppOptions']";
-	private final String COMMENT_FIELD="//textarea[@id='CommentsId']";
-	private final String SAVE_BUTTON="//a[@class='btn_save']";
-	private final String CLOSE_BUTTON="//a[@class='btn_cancel']";
-	private final String CLOSE_X="//button[@title='close']/span[@title='Close']";
-	private final String MILESTONES_TABLE="//table[@class='gs_features_grid gs_milestones_grid']";
-	private final String MILESTONES_TABLE_DATA_GRID=MILESTONES_TABLE+"/tbody";
-	private final String EDIT_LINK_ICON=MILESTONES_TABLE+"/tbody/tr[%d]/td[@class='edit-icon']";
-	private final String DELETE_LINK_ICON=MILESTONES_TABLE+"/tbody/tr[%d]/td[@class='delete-icon']";
-	private final String MILESSTONES_ADD_FRAME="//div[@class='ui-widget-overlay ui-front']";
-	private final String MILESTONE_ROW_CHECK=MILESTONES_TABLE+"/tbody/tr[%d]";
-	private final String MILESTONE_ROW="//table[@class='gs_features_grid gs_milestones_grid']"+
-									   "/tbody/tr/td[1][contains(.,'%s')]"+
-									   "/following-sibling::td[1]/span[@style='background-color:%s;']"+
-									   "/following-sibling::span[contains(.,'%s')]/parent::td"+
-									   "/following-sibling::td[1][contains(.,'%s')]"+
-									   "/following-sibling::td[1][contains(.,'%s')]";
-	private final String NO_OF_ROWS="//table[@class='gs_features_grid gs_milestones_grid']/tbody/tr";
+	private final String NO_MILESTONES_MSG          = "//div[@class='noMilestone noDataFound' and contains(.,'No Milestones Found')]";
+	private final String MILESTONES_SUB_TAB         = "//li[@data-tabname='Milestones']/a[contains(.,'Milestones')]";
+	private final String ADD_MILESTONES             = "//a[@class='addNewMilestone']";
+	private final String DATE_FIELD                 = "//input[@id='DateId']";
+	private final String MILESTONE_DROP_BOX         = "//select[@id='MilestoneOptions']/following-sibling::button";
+	private final String OPPORTUNITY_DROP_BOX       = "//select[@id='OppOptions']/following-sibling::button";
+	private final String COMMENT_FIELD              = "//textarea[@id='CommentsId']";
+	private final String SAVE_BUTTON                = "//a[@class='btn_save']";
+	private final String CLOSE_BUTTON               = "//a[@class='btn_cancel']";
+	private final String CLOSE_X                    = "//button[@title='close']/span[@title='Close']";
+	private final String MILESTONES_TABLE           = "//table[@class='gs_features_grid gs_milestones_grid']";
+	private final String MILESTONES_TABLE_DATA_GRID = MILESTONES_TABLE+"/tbody";
+	private final String EDIT_LINK_ICON             = MILESTONES_TABLE+"/tbody/tr[%d]/td[@class='edit-icon']";
+	private final String DELETE_LINK_ICON           = MILESTONES_TABLE+"/tbody/tr[%d]/td[@class='delete-icon']";
+	private final String MILESTONES_POPUP           = "//div[@class='ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix']/span[contains(text(), 'Milestone')]";
+	private final String MILESTONE_ROW_CHECK        = MILESTONES_TABLE+"/tbody/tr[%d]";
+    private final String NO_OF_ROWS                 = "//table[@class='gs_features_grid gs_milestones_grid']/tbody/tr";
+    private final String MILESTONE_ROW              = "//table[@class='gs_features_grid gs_milestones_grid']"+
+                                                       "/tbody/tr/td[1][contains(.,'%s')]"+
+                                                       "/following-sibling::td[1]/span[@style='background-color:%s;']"+
+                                                       "/following-sibling::span[contains(.,'%s')]/parent::td"+
+                                                       "/following-sibling::td[1][contains(.,'%s')]"+
+                                                       "/following-sibling::td[1][contains(.,'%s')]";
+
 
 	
 	public void gotoMilestonesSubtab(){
@@ -35,7 +36,7 @@ public class Customer360Milestones extends Customer360Page {
 	
 	public void clickOnAddMilestones(){
 		item.click(ADD_MILESTONES);
-		wait.waitTillElementDisplayed(MILESSTONES_ADD_FRAME, MIN_TIME, MAX_TIME);
+		wait.waitTillElementDisplayed(MILESTONES_POPUP, MIN_TIME, MAX_TIME);
 	}
 	
 	public boolean isHeaderPresent(){
@@ -55,27 +56,17 @@ public class Customer360Milestones extends Customer360Page {
 		return true;
 	}
 	public void setDateInField(String date){
-		
-		/*JB-3102 needs to be fixed to select date from date-picker
-		 item.click(DATE_FIELD);
-		//sample for date = "2014-December-13"
-		String dElements[]=date.split("-");
-		item.click(DATE_YEAR);
-		item.click(DATE_MONTH);
-		item.click(DATE_DAY);*/
-		
-		//(OR) simply enter the date in the date field
 		item.clearAndSetText(DATE_FIELD, date);
 	}
 	
 	public void selectMileStone(String Milestone){
 		item.click(MILESTONE_DROP_BOX);
-		item.click(MILESTONE_DROP_BOX+"/option[contains(.,'"+Milestone+"')]");
+		item.click("//ul[@class='ui-multiselect-checkboxes ui-helper-reset']/descendant::span[contains(text(), '"+Milestone+"')]");
 	}
 	
 	public void selectOpportunityForMilestone(String Opportunity){
 		item.click(OPPORTUNITY_DROP_BOX);
-		item.click(OPPORTUNITY_DROP_BOX+"/option[contains(.,'"+Opportunity+"')]");
+        item.click("//ul[@class='ui-multiselect-checkboxes ui-helper-reset']/descendant::span[contains(text(), '"+Opportunity+"')]");
 	}
 	
 	public void addComments(String Comments){
@@ -104,7 +95,7 @@ public class Customer360Milestones extends Customer360Page {
 	
 	public void clickOnEditMilestone(int row){
 		item.click(String.format(EDIT_LINK_ICON,row));
-		wait.waitTillElementDisplayed(MILESSTONES_ADD_FRAME, MIN_TIME, MAX_TIME);
+		wait.waitTillElementDisplayed(MILESTONES_POPUP, MIN_TIME, MAX_TIME);
 	}
 	
 	public void clickOnDeleteMilestone(int row){
