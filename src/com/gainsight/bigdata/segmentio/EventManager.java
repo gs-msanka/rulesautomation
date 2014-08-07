f72f4830ee1c0a5ab308c39cfb213ec7c12f70c4
Srinivas <spulagam@gainsight.com>
2014-08-07 13:37:05 +0530

    segment io integration tests utilities

---
+++ src/com/gainsight/bigdata/segmentio/EventManager.java
@@ -0,0 +1,45 @@
+package com.gainsight.bigdata.segmentio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import com.gainsight.pageobject.core.Report;
+import us.monoid.web.Resty;
+
+public class EventManager {
+	private static final int NUMBER_OF_THREADS = 10;
+
+	public static int submitEvents(Resty resty, String url,
+			List<String> payloads) throws ExecutionException {
+		ExecutorService executor = Executors
+				.newFixedThreadPool(NUMBER_OF_THREADS);
+		List<Future<Boolean>> list = new ArrayList<Future<Boolean>>();
+		for (String payload : payloads) {
+			Callable<Boolean> worker = new EventSubmitter(resty, url, payload);
+			Future<Boolean> submit = executor.submit(worker);
+			list.add(submit);
+		}
+		int successfulEvents = 0;
+		for (Future<Boolean> future : list) {
+			try {
+				if (future.get()) {
+					successfulEvents++;
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			executor.awaitTermination(180L, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			Report.logInfo("Error while waiting for threads to complete event submissions (timeout is 180 seconds)");
+			e.printStackTrace();
+		}
+		return successfulEvents;
+	}
+}
---
+++ src/com/gainsight/bigdata/segmentio/EventSubmitter.java
@@ -0,0 +1,30 @@
+package com.gainsight.bigdata.segmentio;
+
+import java.util.concurrent.Callable;
+import com.gainsight.pageobject.core.Report;
+import us.monoid.web.Resty;
+import static us.monoid.web.Resty.*;
+
+public class EventSubmitter implements Callable<Boolean> {
+    private final Resty resty;
+    private final String url;
+    private final String payload;
+    public EventSubmitter(Resty resty,String url,String payload){
+    	this.url=url;
+    	this.payload=payload;
+    	this.resty=resty;
+    	
+    }
+	@Override
+	public Boolean call() throws Exception {
+		Boolean isSuccess=false;
+		try{
+			isSuccess=Boolean.parseBoolean((String)resty.json(url, content(payload)).get("status"));
+		}
+		catch (Exception e){
+			Report.logInfo("Error when sending event request " +e.getMessage());
+			isSuccess=false;
+		}
+		return isSuccess;
+	}
+}
