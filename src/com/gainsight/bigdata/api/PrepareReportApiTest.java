68dd52bdb0ed11ce9a97b2de83152c089130d6d6
gs-sivaprasad <snasika@gainsight.com>
2014-08-24 23:52:16 +0530

    Report Builder Test Cases updated

--- src/com/gainsight/bigdata/api/PrepareReportApiTest.java
+++ src/com/gainsight/bigdata/api/PrepareReportApiTest.java
@@ -26,132 +26,157 @@ public class PrepareReportApiTest extends TestBase {
 	public void setUp() throws Exception {
 		init();
 
-		uri = PropertyReader.nsAppUrl + "/preparereport";
+		uri = PropertyReader.nsAppUrl + "/api/reports/run/preparation";
 	}
-	
+
+	@Test
+	public void test_2Row() throws Exception {
+		String rawBody = FileUtils.readFileToString(new File(testDataLoc
+				+ "2row_input.txt"));
+		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "2row_output.txt"));
+		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
+				"Expected and Actual Json not match. Format/Values are wrong");
+	}
+
+	@Test
+	public void test_2Mea() throws Exception {
+		String rawBody = FileUtils.readFileToString(new File(testDataLoc
+				+ "2mea_input.txt"));
+		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "2mea_output.txt"));
+		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
+				"Expected and Actual Json not match. Format/Values are wrong");
+	}
+
 	@Test
 	public void test_1Col_1Measure() throws Exception {
 		String rawBody = FileUtils.readFileToString(new File(testDataLoc
 				+ "1col_1measure_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
 		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
-		
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "1col_1measure_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "1col_1measure_output.txt"));
 		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
-		
+				"Expected and Actual Json not match. Format/Values are wrong");
 	}
 
-	@Test
-	public void test_1Measure() throws Exception {
-		String rawBody = FileUtils.readFileToString(new File(testDataLoc
-				+ "1measure_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
-		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
-
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "1measure_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
-		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
-		
-	}
-	
 	@Test
 	public void test_1MeasureOnRow() throws Exception {
 		String rawBody = FileUtils.readFileToString(new File(testDataLoc
 				+ "1measureOnRow_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
 		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
 
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "1measureOnRow_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "1measureOnRow_output.txt"));
+
 		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
-		
+				"Expected and Actual Json not match. Format/Values are wrong");
 	}
-	
+
 	@Test
 	public void test_1Row_1Measure() throws Exception {
 		String rawBody = FileUtils.readFileToString(new File(testDataLoc
 				+ "1row_1measure_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
 		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
 
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "1row_1measure_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "1row_1measure_output.txt"));
 		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
-		
+				"Expected and Actual Json not match. Format/Values are wrong");
 	}
-	
+
 	@Test
 	public void test_1Row_1MeasureOnRow() throws Exception {
 		String rawBody = FileUtils.readFileToString(new File(testDataLoc
 				+ "1row_1measureOnRow_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
 		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
 
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "1row_1measureOnRow_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "1row_1measureOnRow_output.txt"));
 		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
+				"Expected and Actual Json not match. Format/Values are wrong");
 	}
-	
+
 	@Test
 	public void test_2Row_2Col_2Measure() throws Exception {
 		String rawBody = FileUtils.readFileToString(new File(testDataLoc
 				+ "2row_2col_2measure_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
 		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
 
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "2row_2col_2measure_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "2row_2col_2measure_output.txt"));
 		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
-		
+				"Expected and Actual Json not match. Format/Values are wrong");
 	}
-	
+
 	@Test
 	public void test_2Row_2Measure() throws Exception {
 		String rawBody = FileUtils.readFileToString(new File(testDataLoc
 				+ "2row_2measure_input.txt"));
-		Report.logInfo("Request:\n"+rawBody);
 		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
-		Report.logInfo("Actual JSON:\n" + result.getContent());
 
-		String expectedJson = FileUtils.readFileToString(new File(
-				testDataLoc + "2row_2measure_output.txt"));
-		Report.logInfo("Expected JSON:\n" + expectedJson);
-		
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "2row_2measure_output.txt"));
+
 		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
-				"Json Output did not match the expected report. " +
-				"It happens in 2 conditions either the format is wrong or the values are wrong");
-		
+				"Expected and Actual Json not match. Format/Values are wrong");
+	}
+
+	@Test
+	public void test_3Row_1measure_sortOnMeasure() throws Exception {
+		String rawBody = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_1measure_sortOnMeasure_input.txt"));
+		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_1measure_sortOnMeasure_output.txt"));
+
+		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
+				"Expected and Actual Json not match. Format/Values are wrong");
+	}
+
+	@Test
+	public void test_3Row_1measure_sortOnRow() throws Exception {
+		String rawBody = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_1measure_sortOnRow_input.txt"));
+		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_1measure_sortOnRow_output.txt"));
+
+		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
+				"Expected and Actual Json not match. Format/Values are wrong");
+	}
+
+	@Test
+	public void test_3Row_2Col_2measure_sortOnRow() throws Exception {
+		String rawBody = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_2col_2measure_sortOnRow_input.txt"));
+		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_2col_2measure_sortOnRow_output.txt"));
+
+		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
+				"Expected and Actual Json not match. Format/Values are wrong");
+	}
+
+	@Test
+	public void test_1Row_2measures_dimensionOnMeasure() throws Exception {
+		String rawBody = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_1measure_sortOnMeasure_input.txt"));
+		HttpResponseObj result = wa.doPost(uri, rawBody, h.getAllHeaders());
+
+		String expectedJson = FileUtils.readFileToString(new File(testDataLoc
+				+ "3row_1measure_sortOnMeasure_output.txt"));
+
+		Assert.assertTrue(jsonOutputCompare(result.getContent(), expectedJson),
+				"Expected and Actual Json not match. Format/Values are wrong");
 	}
 
 	@AfterClass
@@ -159,17 +184,18 @@ public class PrepareReportApiTest extends TestBase {
 
 	}
 
-	private boolean jsonOutputCompare(String actual, String expected) throws JsonProcessingException, IOException {
+	private boolean jsonOutputCompare(String actual, String expected)
+			throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode tree1 = mapper.readTree(actual);
 		JsonNode tree2 = mapper.readTree(expected);
-		
-		if(tree1.equals(tree2)) {
+
+		if (tree1.equals(tree2)) {
 			Report.logInfo("Report output Matched");
 			return true;
-		}
-		else {
-			Report.logInfo("Report output Did Not Match");
+		} else {
+			Report.logInfo("Report output Did Not Match. Expected JSON:\n"
+					+ expected + "\n Actual JSON:" + actual);
 			return false;
 		}
 	}
