015c80a4ec7aafbe4227111c1d37bbe2f3ccc2cd
gs-sivaprasad <snasika@gainsight.com>
2014-09-20 16:50:38 +0530

    Updated Upsert Report Test Cases

--- src/com/gainsight/bigdata/api/UpsertReportTest.java
+++ src/com/gainsight/bigdata/api/UpsertReportTest.java
@@ -10,29 +10,31 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.gainsight.bigdata.TestBase;
+import com.gainsight.bigdata.util.ApiUrl;
 import com.gainsight.bigdata.util.PropertyReader;
 import com.gainsight.pageobject.core.Report;
-import com.gainsight.pojo.Header;
 import com.gainsight.pojo.HttpResponseObj;
 
 public class UpsertReportTest extends TestBase {
 
 	String baseuri;
 	String testDataLoc = testDataBasePath + "/UpsertReportTestData/";
-	String rid = "f4e1f1a7-c887-4972-93e5-d446d231b77e";
-	
+	String rid = "750dc4c2-d2f1-4b28-bf3a-80893c49e183";
+	long epoch = System.currentTimeMillis() / 1000;
+
 	@BeforeClass
 	public void setUp() throws Exception {
 		init();
-		baseuri = PropertyReader.nsAppUrl + "/upsertreport";
+		baseuri = PropertyReader.nsAppUrl + ApiUrl.REPORT_SAVE;
 	}
-	
+
 	@Test
-	public void upsertReport() throws Exception {
+	public void saveReport() throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		String rawBody = mapper.readTree(new File(testDataLoc + "UpsertReportInput1.txt")).toString();
+		rawBody = rawBody.replaceFirst("\"reportName\":\"\"", "\"reportName\":\"New Report" + epoch + "\"");
 		Report.logInfo(rawBody);
-		HttpResponseObj result = wa.doPost(baseuri, rawBody, h.getAllHeaders());
+		HttpResponseObj result = wa.doPut(baseuri, rawBody, h.getAllHeaders());
 		Report.logInfo(result.toString());
 		JsonNode reportId = mapper.readTree(result.getContent());
 		JsonNode data = reportId.findPath("data");
@@ -40,73 +42,60 @@ public class UpsertReportTest extends TestBase {
 		Report.logInfo(rid);
 		Assert.assertNotNull(reportId, "Report ID not found.");
 	}
-	
+
 	@Test
-	public void upsertExistingReport() throws Exception {
+	public void saveAsReport() throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		String rawBody = mapper.readTree(new File(testDataLoc + "UpsertReportInput2.txt")).toString();
-		rawBody = rawBody.replaceFirst("\"ReportId\":\"\"", "\"ReportId\":\""+rid+"\"");
+		rawBody = rawBody.replaceFirst("\"ReportId\":\"\"", "\"ReportId\":\"" + rid + "\"");
+		rawBody = rawBody.replaceFirst("Automation Report2", "RenamedReport" + epoch);
 		Report.logInfo(rawBody);
-		HttpResponseObj result = wa.doPost(baseuri, rawBody, h.getAllHeaders());
+		HttpResponseObj result = wa.doPut(baseuri, rawBody, h.getAllHeaders());
 		Report.logInfo(result.toString());
 		JsonNode reportId = mapper.readTree(result.getContent());
 		JsonNode data = reportId.findPath("data");
 		Report.logInfo(data.get("ReportId").toString());
 		Assert.assertNotNull(reportId, "Report ID not found.");
 	}
-	
+
 	@Test
-	public void upsertExistingReportWithEmptyReportId() throws Exception {
+	public void updateReport() throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		String rawBody = mapper.readTree(new File(testDataLoc + "UpsertReportInput2.txt")).toString();
 		Report.logInfo(rawBody);
-		HttpResponseObj result = wa.doPost(baseuri, rawBody, h.getAllHeaders());
+		HttpResponseObj result = wa.doPut(baseuri, rawBody, h.getAllHeaders());
 		Report.logInfo(result.toString());
 		JsonNode reportId = mapper.readTree(result.getContent());
 		JsonNode data = reportId.findPath("data");
 		Report.logInfo(data.get("ReportId").toString());
-		Assert.assertNull(reportId, "Report ID found instead of an Error");
+		Assert.assertNotNull(reportId, "Report ID not found.");
 	}
-	
+
 	@Test
-	public void upsertReportWithEmptyRequest() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		String rawBody = "";
-		Report.logInfo(rawBody);
-		HttpResponseObj result = wa.doPost(baseuri, rawBody, h.getAllHeaders());
-		Report.logInfo(result.toString());
-		Assert.assertNotEquals(result.getStatusCode(), 200, "Response was 200 OK");
-	}
-	
-	@Test
-	public void upsertReportWithInvalidJson() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		String rawBody = "sdgfhghjkml345678";
-		Report.logInfo(rawBody);
-		HttpResponseObj result = wa.doPost(baseuri, rawBody, h.getAllHeaders());
-		Report.logInfo(result.toString());
-		Assert.assertNotEquals(result.getStatusCode(), 200, "Response was 200 OK");
-	}
-	
-	@Test
-	public void upsertReportWithInvalidAuthHeader() throws Exception {
+	public void upsertExistingReportWithEmptyReportId() throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		String rawBody = mapper.readTree(new File(testDataLoc + "UpsertReportInput2.txt")).toString();
+		rawBody = rawBody.replaceFirst("cf286c13-2371-436f-a9b9-85636968717a", "");
 		Report.logInfo(rawBody);
-		
-		Header h = new Header();
-		h.addHeader("Content-Type", "application/json");
-		h.addHeader("authToken", "AddingGarbage");
-		h.addHeader("Origin", origin);
-		
-		HttpResponseObj result = wa.doPost(baseuri, rawBody, h.getAllHeaders());
+		HttpResponseObj result = wa.doPut(baseuri, rawBody, h.getAllHeaders());
 		Report.logInfo(result.toString());
-		Assert.assertTrue(result.getContent().equals("Invalid AuthToken"), "Invalid Auth Header is accepted");
+		JsonNode reportId = mapper.readTree(result.getContent());
+		Report.logInfo(reportId.get("result").toString());
+		Assert.assertEquals(reportId.get("result").toString(), "false", "Report ID found instead of an Error");
 	}
-	
+
+	@Test
+	public void upsertReportWithEmptyRequest() throws Exception {
+		String rawBody = "";
+		Report.logInfo(rawBody);
+		HttpResponseObj result = wa.doPut(baseuri, rawBody, h.getAllHeaders());
+		Report.logInfo(result.toString());
+		Assert.assertNotEquals(result.getStatusCode(), 200, "Response was 200 OK");
+	}
+
 	@AfterClass
 	public void tearDown() {
-		
+
 	}
 
 }
