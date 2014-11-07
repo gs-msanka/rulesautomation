862394038522fd2378745cc1a17a04c41f47db25
gs-golla <ggolla@gainsight.com>
2014-11-07 11:51:44 +0530

    Adding Workflow Pages.

--- src/com/gainsight/sfdc/workflow/pages/OperationalReportingPage.java
+++
@@ -1,5 +0,0 @@
-package com.gainsight.sfdc.workflow.pages;
-
-public class OperationalReportingPage {
-
-}
---
+++ src/com/gainsight/sfdc/workflow/pages/WorkflowCalendarPage.java
@@ -0,0 +1,7 @@
+package com.gainsight.sfdc.workflow.pages;
+
+/**
+ * Created by gainsight on 07/11/14.
+ */
+public class WorkflowCalendarPage {
+}
