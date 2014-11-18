edb2d1225dc2ff51a89a2fb32aca9533c8f94e74
Swetha <sparava@gainsight.com>
2014-11-18 11:13:42 +0530

    Rules - GS email action testcases

---
+++ src/com/gainsight/bigdata/rulesengine/SendEmail.java
@@ -0,0 +1,104 @@
+package com.gainsight.bigdata.rulesengine;
+
+import com.gainsight.bigdata.util.PropertyReader;
+import com.gainsight.pojo.Header;
+import com.gainsight.pojo.HttpResponseObj;
+import com.gainsight.sfdc.util.bulk.SFDCUtil;
+import com.gainsight.utils.SOQLUtil;
+import com.gainsight.webaction.WebAction;
+import com.sforce.soap.partner.sobject.SObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by sparava on 11/17/14.
+ */
+public class SendEmail {
+    public WebAction webAction = new WebAction();
+    public SFDCUtil sfdc = new SFDCUtil();
+    public Header header = new Header();
+    public SOQLUtil soql = new SOQLUtil();
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        LoginUtil.sfdcLogin(soql, header, webAction);
+    }
+
+    @Test
+    public void testGSEmailAccountStrategy() throws Exception {
+
+        SObject[] rules = soql
+                .getRecords("select Id,Name from JBCXM__AutomatedAlertRules__c where Name='Send Email Account Strategy'");
+        for (SObject r : rules) {
+            String rawBody = ("{}");
+            HttpResponseObj result = webAction.doPost(PropertyReader.nsAppUrl + "/api"
+                            + "/eventrule" + "/" + r.getId() + "", rawBody,
+                    header.getAllHeaders());
+            ResponseObject responseObj = LoginUtil.convertToObject(result
+                    .getContent());
+            Assert.assertTrue(Boolean.valueOf(responseObj.getResult()));
+            Assert.assertNotNull(responseObj.getRequestId());
+            Thread.sleep(20000);
+
+            SObject[] res = soql
+                    .getRecords("select JBCXM__LastRunResult__c from JBCXM__AutomatedAlertRules__c where Id='" + r.getId() + "'");
+            for (SObject obj : res) {
+                Assert.assertNotNull(obj.getChild("JBCXM__LastRunResult__c").getValue());
+                Assert.assertEquals("success", obj.getChild("JBCXM__LastRunResult__c").getValue().toString().toLowerCase());
+            }
+        }
+    }
+
+
+    @Test
+    public void testGSEmailContactStrategy() throws Exception {
+
+        SObject[] rules = soql
+                .getRecords("select Id,Name from JBCXM__AutomatedAlertRules__c where Name='Send Email Contact Strategy'");
+        for (SObject r : rules) {
+            String rawBody = ("{}");
+            HttpResponseObj result = webAction.doPost(PropertyReader.nsAppUrl + "/api"
+                            + "/eventrule" + "/" + r.getId() + "", rawBody,
+                    header.getAllHeaders());
+            ResponseObject responseObj = LoginUtil.convertToObject(result
+                    .getContent());
+            Assert.assertTrue(Boolean.valueOf(responseObj.getResult()));
+            Assert.assertNotNull(responseObj.getRequestId());
+            Thread.sleep(20000);
+
+            SObject[] res = soql
+                    .getRecords("select JBCXM__LastRunResult__c from JBCXM__AutomatedAlertRules__c where Id='" + r.getId() + "'");
+            for (SObject obj : res) {
+                Assert.assertNotNull(obj.getChild("JBCXM__LastRunResult__c").getValue());
+                Assert.assertEquals("success", obj.getChild("JBCXM__LastRunResult__c").getValue().toString().toLowerCase());
+            }
+        }
+    }
+
+    @Test
+    public void testGSEmailEmailStrategy() throws Exception {
+
+        SObject[] rules = soql
+                .getRecords("select Id,Name from JBCXM__AutomatedAlertRules__c where Name='Send Email Email Strategy'");
+        for (SObject r : rules) {
+            String rawBody = ("{}");
+            HttpResponseObj result = webAction.doPost(PropertyReader.nsAppUrl + "/api"
+                            + "/eventrule" + "/" + r.getId() + "", rawBody,
+                    header.getAllHeaders());
+            ResponseObject responseObj = LoginUtil.convertToObject(result
+                    .getContent());
+            Assert.assertTrue(Boolean.valueOf(responseObj.getResult()));
+            Assert.assertNotNull(responseObj.getRequestId());
+            Thread.sleep(20000);
+
+            SObject[] res = soql
+                    .getRecords("select JBCXM__LastRunResult__c from JBCXM__AutomatedAlertRules__c where Id='" + r.getId() + "'");
+            for (SObject obj : res) {
+                Assert.assertNotNull(obj.getChild("JBCXM__LastRunResult__c").getValue());
+                Assert.assertEquals("success", obj.getChild("JBCXM__LastRunResult__c").getValue().toString().toLowerCase());
+            }
+        }
+    }
+
+}
