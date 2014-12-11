a1406763ffe2c869bc072ae1fdb18aa2d9afbf53
gs-golla <ggolla@gainsight.com>
2014-12-11 10:27:48 +0530

    Un commented Setup in account weekly configuration.

--- src/com/gainsight/sfdc/adoption/tests/Adoption_Account_Weekly_Test.java
+++ src/com/gainsight/sfdc/adoption/tests/Adoption_Account_Weekly_Test.java
@@ -25,13 +25,13 @@ public class Adoption_Account_Weekly_Test extends AdoptionDataSetup {
     public void setUp() throws IOException {
         basepage.login();
         dataSetup = new AdoptionDataSetup();
-        /*apex.runApexCodeFromFile(advUsageConfigFile, isPackage);
+        apex.runApexCodeFromFile(advUsageConfigFile, isPackage);
         dataSetup.initialSetup();
         dataSetup.loadUsageAccountAndCustomersData();
         dataSetup.loadUsageData(JOB_UsageData);
         dataSetup.updateUtilizationCal("STANDARD");
         dataSetup.updateUsersDisplayInUsageGrids(true);
-        dataSetup.runAdoptionAggregation(1, true, isEndDate, WEEKDAY); */
+        dataSetup.runAdoptionAggregation(1, true, isEndDate, WEEKDAY);
     }
 
     @Test(dataProviderClass = com.gainsight.utils.ExcelDataProvider.class, dataProvider = "excel")
