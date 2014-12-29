8526c1b88e0bfe864f1e9aa599dc9b9e4f51bc7f
gs-golla <ggolla@gainsight.com>
2014-12-29 10:56:55 +0530

    File paths updates.

--- src/com/gainsight/sfdc/rulesEngine/tests/Rule_Instance_Monthly_Test.java
+++ src/com/gainsight/sfdc/rulesEngine/tests/Rule_Instance_Monthly_Test.java
@@ -35,18 +35,18 @@ import com.sforce.ws.ConnectionException;
  * To change this template use File | Settings | File Templates.
  */
 public class Rule_Instance_Monthly_Test extends BaseTest {
-    private static final String SET_USAGE_DATA_LEVEL_FILE = Application.basedir+"/testdata/sfdcrulesEngineScripts/Set_Instance_Level_Monthly.apex";
-    private static final String SET_USAGE_DATA_MEASURE_FILE = Application.basedir+"/testdata/sfdcrulesEngineScripts/UsageData_Measures.apex";
-    private static final String USAGE_DATA_FILE         = "/testdata/sfdcrulesEngineData/Rules_UsageData_Instance.csv";
-    private static final String TEST_DATA_FILE          = "testdata/sfdcrulesEngineTests/Rule_Instance_Monthly_Test.xls";
+    private static final String SET_USAGE_DATA_LEVEL_FILE = Application.basedir+"/testdata/sfdc/rulesEngine/scripts/Set_Instance_Level_Monthly.apex";
+    private static final String SET_USAGE_DATA_MEASURE_FILE = Application.basedir+"/testdata/sfdc/rulesEngine/scripts/UsageData_Measures.apex";
+    private static final String USAGE_DATA_FILE         = "/testdata/sfdc/rulesEngine/data/Rules_UsageData_Instance.csv";
+    private static final String TEST_DATA_FILE          = "testdata/sfdc/rulesEngine/tests/Rule_Instance_Monthly_Test.xls";
     private static final String AUTOMATED_RULE_OBJECT   = "JBCXM__AutomatedAlertrules__c";
     private static final String ALERT_CRITERIA_KEY      = "JBCXM__AlertCriteria__c";
     private static final String SCORE_CRITERIA_KEY      = "JBCXM__ScorecardCriteria__c";
-    private static final String NUMERIC_SCHEME_FILE     = Application.basedir+"/apex_scripts/Scorecard/Scorecard_enable_numeric.apex";
-    private static final String METRICS_CREATE_FILE     = Application.basedir+"/apex_scripts/Scorecard/Create_ScorecardMetrics.apex";
-    private static final String SCORECARD_CLEAN_FILE    = Application.basedir+"/apex_scripts/Scorecard/Scorecard_CleanUp.txt";
-    private final static String JOB_ACCOUNT_LOAD        = Application.basedir + "/testdata/sfdcrulesEngineJobs/Job_Accounts.txt";
-    private final static String JOB_CUSTOMER_LOAD       = Application.basedir + "/testdata/sfdcrulesEngineJobs/Job_Customers.txt";
+    private static final String NUMERIC_SCHEME_FILE     = Application.basedir+"/apex_scripts/scorecard/Scorecard_enable_numeric.apex";
+    private static final String METRICS_CREATE_FILE     = Application.basedir+"/apex_scripts/scorecard/Create_ScorecardMetrics.apex";
+    private static final String SCORECARD_CLEAN_FILE    = Application.basedir+"/apex_scripts/scorecard/Scorecard_CleanUp.txt";
+    private final static String JOB_ACCOUNT_LOAD        = Application.basedir + "/testdata/sfdc/rulesEngine/jobs/Job_Accounts.txt";
+    private final static String JOB_CUSTOMER_LOAD       = Application.basedir + "/testdata/sfdc/rulesEngine/jobs/Job_Customers.txt";
 
     private static SFDCInfo sfdcInfo = SFDCUtil.fetchSFDCinfo();
     private RuleEngineDataSetup ruleEngineDataSetup;
