7911aefc855aa4150063705d46324cb6ccfb8cc0
gs-jvunnam <jvunnam@gainsight.com>
2015-01-20 16:33:20 +0530

    Adding accounts and customers for Survey

--- src/com/gainsight/sfdc/survey/pages/SurveyAddParticipantsPage.java
+++ src/com/gainsight/sfdc/survey/pages/SurveyAddParticipantsPage.java
@@ -1,15 +1,19 @@
 package com.gainsight.sfdc.survey.pages;
 
+import org.testng.Assert;
+
+import com.gainsight.testdriver.Log;
+
 /**
  * Created by gainsight on 05/12/14.
  */
-public class SurveyAddParticipantsPage {
+public class SurveyAddParticipantsPage extends SurveyBasePage {
 	
 	private final String ADD_PARTICIPANTS_PAGE = "//a[contains(@class,'sub-menu-option  sub-opt-addparticipants')]";
 	private final String SEARCH = "//li[contains(@class,'1-search')]"; //Click on Search link
 	private final String ADD_PARTICIPANTS = "//li[contains(@class,'2-add-participants')]"; //Click on Add Participants link
 	
-	//Objects uunder Search Section
+	//Objects under Search Section
 	private final String LOAD_SURVEY_PARTICIPANTS_FROM = "//span[contains(text(),'Contact Object')]|//span[contains(text(),'Custom Object')]|//span[contains(text(),'CSV File')]";
 	
 	private final String SELECT_ROLE_CONTACT = "//input[@class='contact-type frmcontact' and @type='radio']";
@@ -32,10 +36,40 @@ public class SurveyAddParticipantsPage {
 	private final String SAVE_AND_SHOW_CONTACTS_BTN = "//a[contains(text(),'Save & Show Contacts')]";
 	
 	//Objects under Add Participants Section
-	private final String SELECT_ALL = "//input[@id='isLoadAll']"; 
-	private final String ADD_TO_DISTRIBUTION_LIST = "//a[@class='primary-btn mgrn add-distribution' and contains(text(),'Add to Distribution List')]";
+	private final String SELECT_ALL = "//input[@id='isLoadAll']";
 	
 	private final String SELECT_ALL_DISPLAYED_IN_GRID = "//input[@id='cb_show-contacts-tbl']";
+	private final String ADD_TO_DISTRIBUTION_LIST = "//a[@class='primary-btn mgrn add-distribution' and contains(text(),'Add to Distribution List')]";
 	
-			
+	private final String SUCCESSFUL_MSG = "//div[contains(text(),'Selected participants are added successfully')]";
+	
+	
+	public SurveyAddParticipantsPage() {
+		wait.waitTillElementPresent(ADD_PARTICIPANTS_PAGE, MIN_TIME, MAX_TIME);
+	}
+	
+	//Method to add filters 
+	public void addFilter(String fieldName,String Operator,String Value){
+		item.selectFromDropDown(SELECT_A_FIELD, fieldName);
+		item.selectFromDropDown(OPERATOR, Operator);
+		field.setText(VALUE, Value);
+		Log.info("Added filters");
+	}
+	
+	//Method to navigate from Search page to Add Participants page
+	public void navigateToAddParticipantsPage(){
+		button.click(SAVE_AND_SHOW_CONTACTS_BTN);
+		wait.waitTillElementPresent(ADD_TO_DISTRIBUTION_LIST, MIN_TIME, MAX_TIME);
+		Log.info("Navigated to Add Participants page");
+	}
+	
+	public void verifySuccessfulMessage(){
+		Log.info("Verify the successful message after adding the contacts to the distribution list");
+		wait.waitTillElementPresent(SUCCESSFUL_MSG, MIN_TIME, MAX_TIME);
+		
+	}
+	
+	
+	
+		
 }
---
+++ src/com/gainsight/sfdc/survey/tests/SurveyAddParticipantsTest.java
@@ -0,0 +1,7 @@
+package com.gainsight.sfdc.survey.tests;
+
+import com.gainsight.sfdc.tests.BaseTest;
+
+public class SurveyAddParticipantsTest extends BaseTest {
+
+}
