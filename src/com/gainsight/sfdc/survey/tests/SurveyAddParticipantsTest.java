4db9161175631f9ae6b7b04db3fc677170161cc9
gs-jvunnam <jvunnam@gainsight.com>
2015-01-21 12:26:57 +0530

    Added jobs and test data to Survey Add participants

--- src/com/gainsight/sfdc/survey/tests/SurveyAddParticipantsTest.java
+++ src/com/gainsight/sfdc/survey/tests/SurveyAddParticipantsTest.java
@@ -1,7 +1,26 @@
 package com.gainsight.sfdc.survey.tests;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.annotations.BeforeClass;
+
 import com.gainsight.sfdc.tests.BaseTest;
+import com.gainsight.sfdc.util.datagen.DataETL;
+import com.gainsight.sfdc.util.datagen.JobInfo;
 
 public class SurveyAddParticipantsTest extends BaseTest {
 
+	private final String CREATE_ACCS=env.basedir+"/testdata/sfdc/survey/scripts/Create_Accounts_Customers_For_Survey.txt";
+	
+	@BeforeClass
+	public void setup() throws Exception {
+		sfdc.connect();
+		basepage.login();
+		DataETL dataLoader = new DataETL();
+        ObjectMapper mapper = new ObjectMapper();
+        createExtIdFieldOnAccount();
+        sfdc.runApexCode(getNameSpaceResolvedFileContents(CREATE_ACCS));
+        
+        JobInfo loadContacts = mapper.readValue(resolveNameSpace(env.basedir+"/testdata/sfdc/survey/jobs/Job_Contacts_DataLoad.txt"), JobInfo.class);
+        dataLoader.execute(loadContacts);
+	}
 }
