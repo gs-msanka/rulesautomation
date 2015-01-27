e45c4233afbb511fdfed10f1130808229beaa5c3
gs-golla <ggolla@gainsight.com>
2015-01-27 15:59:27 +0530

    Null Pointer issue fix.

--- src/com/gainsight/sfdc/workflow/tests/WorkflowSetup.java
+++ src/com/gainsight/sfdc/workflow/tests/WorkflowSetup.java
@@ -250,11 +250,14 @@ public class WorkflowSetup extends BaseTest{
     }
 
     public String getHighestTaskDate(List<Task> tasks) {
-        if((tasks.size() ==0)) {
-            throw new RuntimeException("List should have one task.");
-        }
-        java.util.Date date = null;
+        java.util.Date date;
         SimpleDateFormat dateFormat = new SimpleDateFormat(USER_DATE_FORMAT);
+        try {
+            date = dateFormat.parse(tasks.get(0).getDate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Please check the date format "+e.getErrorOffset());
+        }
         for(Task t : tasks) {
             try {
                 java.util.Date temp = dateFormat.parse(t.getDate());
