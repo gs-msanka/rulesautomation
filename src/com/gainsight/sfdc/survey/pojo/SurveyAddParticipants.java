e09563f68ef7a8048d3749e0070a20c625e59ced
Abhilash Thaduka <athaduka@gainsight.com>
2015-06-12 14:23:25 +0530

    committing change

--- src/com/gainsight/sfdc/survey/pojo/SurveyAddParticipants.java
+++ src/com/gainsight/sfdc/survey/pojo/SurveyAddParticipants.java
@@ -11,8 +11,8 @@ public class SurveyAddParticipants implements Cloneable {
 
 	private String loadParticipantsFrom;
 	private String selectRole;
-	private String SelectRoleField;
-	private String EmailSearch="gainsight.com";
+	private String selectRoleField;
+	private String searchFilter;
 	private String filter;
 	private String operator;
 	private String value;
@@ -20,110 +20,170 @@ public class SurveyAddParticipants implements Cloneable {
 	private String excludeParticipants_Type;
 	private String excludePtp_Survey;
 	private boolean loadAll;
-	private String customobjectname;
-	private String DisplayName;
-	private String DisplayEmail;
-	private String DisplayRole;
+	private String customObjectName;
+	private String displayName;
+	private String displayEmail;
+	private String displayRole;
+	private String contactID;
+	private String contactEmail;
+	private String contactRole;
+	private String contactName;
+	
 
 	private List<ParticipantDetails> participantsList = new ArrayList<ParticipantDetails>();
 	
-	public void setcustomobjectname(String customobjectname) {
-		this.customobjectname = customobjectname;
+	public void setCustomObjectName(String customObjectName) {
+		this.customObjectName = customObjectName;
 	}
-	
-	public String getcustomobjectname() {
-		return customobjectname;
+
+	public String getCustomObjectName() {
+		return customObjectName;
 	}
-	
+
 	public boolean isLoadAll() {
 		return loadAll;
 	}
+
 	public void setLoadAll(boolean loadAll) {
 		this.loadAll = loadAll;
 	}
+
+	public String getContactID() {
+		return contactID;
+	}
+
+	public void setContactID(String contactID) {
+		this.contactID = contactID;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public String getContactRole() {
+		return contactRole;
+	}
+
+	public void setContactRole(String contactRole) {
+		this.contactRole = contactRole;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
 	public String getExcludePtp_Survey() {
 		return excludePtp_Survey;
 	}
+
 	public void setExcludePtp_Survey(String excludePtp_Survey) {
 		this.excludePtp_Survey = excludePtp_Survey;
 	}
+
 	public String getLoadParticipantsFrom() {
 		return loadParticipantsFrom;
 	}
+
 	public void setLoadParticipantsFrom(String loadParticipantsFrom) {
 		this.loadParticipantsFrom = loadParticipantsFrom;
 	}
+
 	public String getSelectRole() {
 		return selectRole;
 	}
+
 	public void setSelectRole(String selectRole) {
 		this.selectRole = selectRole;
 	}
+
 	public String getSelectRoleField() {
-		return SelectRoleField;
+		return selectRoleField;
 	}
-	public void setSelectRoleField(String SelectRoleField) {
-		this.SelectRoleField = SelectRoleField;
+
+	public void setSelectRoleField(String selectRoleField) {
+		this.selectRoleField = selectRoleField;
 	}
-    
-	public String getEmailSearch() {
-		return EmailSearch;
+
+	public String getSearchFilter() {
+		return searchFilter;
 	}
-	public void setEmailSearch(String EmailSearch) {
-		this.EmailSearch = EmailSearch;
+
+	public void setSearchFilter(String searchFilter) {
+		this.searchFilter = searchFilter;
 	}
+
 	public String getOperator() {
 		return operator;
 	}
+
 	public void setOperator(String operator) {
 		this.operator = operator;
 	}
+
 	public String getValue() {
 		return value;
 	}
+
 	public void setValue(String value) {
 		this.value = value;
 	}
+
 	public String getAdvancedLogic() {
 		return advancedLogic;
 	}
+
 	public void setAdvancedLogic(String advancedLogic) {
 		this.advancedLogic = advancedLogic;
 	}
+
 	public String getExcludeParticipants_Type() {
 		return excludeParticipants_Type;
 	}
+
 	public void setExcludeParticipants_Type(String excludeParticipants_Type) {
 		this.excludeParticipants_Type = excludeParticipants_Type;
 	}
+
 	public String getDisplayName() {
-		return DisplayName;
+		return displayName;
 	}
-	public void setDisplayName(String DisplayName) {
-		this.DisplayName = DisplayName;
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
 	}
 
 	public String getDisplayEmail() {
-		return DisplayEmail;
+		return displayEmail;
 	}
-	public void setDisplayEmail(String DisplayEmail) {
-		this.DisplayEmail = DisplayEmail;
+
+	public void setDisplayEmail(String displayEmail) {
+		this.displayEmail = displayEmail;
 	}
-	
+
 	public String getDisplayRole() {
-		return DisplayRole;
+		return displayRole;
 	}
-	public void setDisplayRole(String DisplayRole) {
-		this.DisplayRole = DisplayRole;
+
+	public void setDisplayRole(String displayRole) {
+		this.displayRole = displayRole;
 	}
-	
+
 	public String getFilter() {
 		return filter;
 	}
+
 	public void setFilter(String filter) {
 		this.filter = filter;
 	}
-	
+
 	public List<ParticipantDetails> getParticipantsList() {
 		return participantsList;
 	}
