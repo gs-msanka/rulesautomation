feeff80ad0da1fc9fcf2f9ef405afd19a706e90c
Abhilash Thaduka <athaduka@gainsight.com>
2015-12-17 11:48:57 +0530

    Fixed

---
+++ src/com/gainsight/bigdata/rulesengine/tests/CalculatedFieldsAndMeasuresTest.java
@@ -0,0 +1,179 @@
+package com.gainsight.bigdata.rulesengine.tests;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import com.gainsight.bigdata.NSTestBase;
+import com.gainsight.bigdata.dataload.enums.DataLoadOperationType;
+import com.gainsight.bigdata.dataload.pojo.DataLoadMetadata;
+import com.gainsight.bigdata.gsData.apiImpl.GSDataImpl;
+import com.gainsight.bigdata.pojo.CollectionInfo;
+import com.gainsight.bigdata.pojo.NsResponseObj;
+import com.gainsight.bigdata.rulesengine.RulesUtil;
+import com.gainsight.bigdata.rulesengine.pages.RulesManagerPage;
+import com.gainsight.bigdata.rulesengine.pojo.RulesPojo;
+import com.gainsight.bigdata.rulesengine.pojo.setupaction.FieldMapping;
+import com.gainsight.bigdata.rulesengine.pojo.setupaction.LoadToMDAAction;
+import com.gainsight.bigdata.rulesengine.pojo.setupaction.LoadToSFDCAction;
+import com.gainsight.bigdata.rulesengine.util.RulesEngineUtil;
+import com.gainsight.bigdata.util.CollectionUtil;
+import com.gainsight.http.Header;
+import com.gainsight.sfdc.tests.BaseTest;
+import com.gainsight.sfdc.util.FileUtil;
+import com.gainsight.sfdc.util.datagen.DataETL;
+import com.gainsight.sfdc.util.datagen.FileProcessor;
+import com.gainsight.sfdc.util.datagen.JobInfo;
+import com.gainsight.testdriver.Application;
+import com.gainsight.testdriver.Log;
+import com.gainsight.util.Comparator;
+import com.gainsight.utils.annotations.TestInfo;
+
+/**
+ * @author Abhilash Thaduka
+ *
+ */
+public class CalculatedFieldsAndMeasuresTest extends BaseTest {
+	
+	private static final String CREATE_ACCOUNTS_CUSTOMERS = Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-Scripts/Create_Accounts_Customers.txt";
+	private static final String CLEANUP_SCRIPT = Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-Scripts/Cleanup2.apex";
+	private NSTestBase nsTestBase = new NSTestBase();
+	private ObjectMapper mapper = new ObjectMapper();
+	private DataETL dataETL = new DataETL();
+	private RulesManagerPage rulesManagerPage;
+	private String rulesManagerPageUrl;
+	private RulesEngineUtil rulesEngineUtil = new RulesEngineUtil();
+	private RulesUtil rulesUtil = new RulesUtil();
+	private Header header = new Header();
+	GSDataImpl gsDataImpl = null;
+	private Date date = Calendar.getInstance().getTime();
+	private final String CUSTOM_OBJECT_CLEANUP = "Delete [SELECT Id FROM RulesSFDCCustom__c];";
+	
+	@BeforeClass
+	public void setup() throws Exception {
+		basepage.login();
+		sfdc.connect();
+		nsTestBase.init();
+		rulesManagerPageUrl = visualForcePageUrl + "Rulesmanager";
+		rulesManagerPage = new RulesManagerPage();
+		rulesManagerPage = new RulesManagerPage();
+		header.addHeader("Origin", sfdcInfo.getEndpoint());
+		header.addHeader("Content-Type", "application/json");
+		header.addHeader("appOrgId", sfdcInfo.getOrg());
+		header.addHeader("appUserId", sfdcInfo.getUserId());
+		header.addHeader("appSessionId", sfdcInfo.getSessionId());
+		gsDataImpl = new GSDataImpl(header);
+/*		sfdc.runApexCode(getNameSpaceResolvedFileContents(CREATE_ACCOUNTS_CUSTOMERS));
+		sfdc.runApexCode("Delete [SELECT Id FROM C_Custom__c];");
+		
+		JobInfo jobInfo = mapper.readValue(resolveNameSpace(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4200/LoadDataIntoCustomObject.txt"),JobInfo.class);
+		dataETL.execute(jobInfo);*/
+	}
+
+	@BeforeMethod
+	public void cleanup() {
+		sfdc.runApexCode(getNameSpaceResolvedFileContents(CLEANUP_SCRIPT));
+		sfdc.runApexCode(CUSTOM_OBJECT_CLEANUP);
+
+	}
+
+	@TestInfo(testCaseIds = { "GS-4200" })
+	@Test(description = "Test case to verify if the calculated Fields for comparision calculation type with percentage is working fine when Fields A and B are from Show Fields")
+	public void testCalculatedFields() throws Exception {
+		RulesPojo rulesPojo = mapper.readValue(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4200/GS-4200-Input.json"), RulesPojo.class);
+		rulesManagerPage.openRulesManagerPage(rulesManagerPageUrl);
+		rulesManagerPage.clickOnAddRule();
+		rulesEngineUtil.createRuleFromUi(rulesPojo);
+		Assert.assertTrue(rulesUtil.runRule(rulesPojo.getRuleName()), "Check whether Rule ran successfully or not !");
+		
+		//Verifying the agrregated field vales with the expected data and actual aggregated data
+		dataETL.execute(mapper.readValue(resolveNameSpace(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4200/GS-4200-ExpectedJob.txt"),JobInfo.class));	
+		List<Map<String, String>> expectedData = Comparator.getParsedCsvData(new CSVReader(new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4200/ExpectedData.csv")));
+		List<Map<String, String>> actualData = Comparator.getParsedCsvData(new CSVReader(new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4200/ActualData.csv")));
+		List<Map<String, String>> differenceData = Comparator.compareListData(expectedData, actualData);
+		Log.info("Actual : " + mapper.writeValueAsString(actualData));
+		Log.info("Expected : " + mapper.writeValueAsString(expectedData));
+		Log.info("Difference is : " + mapper.writeValueAsString(differenceData));
+		Assert.assertEquals(differenceData.size(), 0, "Check the Diff above for which the aggregated data is not matching");
+
+	}
+
+	@TestInfo(testCaseIds = { "GS-4204" })
+	@Test(description = "Test case to verify if the calculation type is comparision when Both fields are aggregated over time")
+	public void testCalculatedFields2() throws Exception {
+		RulesPojo rulesPojo = mapper.readValue(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4204/GS-4204-Input.json"), RulesPojo.class);
+		rulesManagerPage.openRulesManagerPage(rulesManagerPageUrl);
+		rulesManagerPage.clickOnAddRule();
+		rulesEngineUtil.createRuleFromUi(rulesPojo);
+		Assert.assertTrue(rulesUtil.runRule(rulesPojo.getRuleName()), "Check whether Rule ran successfully or not !");
+		
+		//Verifying the agrregated field vales with the expected data and actual aggregated data
+		dataETL.execute(mapper.readValue(resolveNameSpace(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4204/GS-4204-ExpectedJob.txt"),JobInfo.class));	
+		List<Map<String, String>> expectedData = Comparator.getParsedCsvData(new CSVReader(new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4204/ExpectedData.csv")));
+		List<Map<String, String>> actualData = Comparator.getParsedCsvData(new CSVReader(new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4204/ActualData.csv")));
+		List<Map<String, String>> differenceData = Comparator.compareListData(expectedData, actualData);
+		Log.info("Actual : " + mapper.writeValueAsString(actualData));
+		Log.info("Expected : " + mapper.writeValueAsString(expectedData));
+		Log.info("Difference is : " + mapper.writeValueAsString(differenceData));
+		Assert.assertEquals(differenceData.size(), 0, "Check the Diff above for which the aggregated data is not matching");
+
+	}
+	
+	@TestInfo(testCaseIds = { "GS-4232" })
+	@Test(description = "Test case to verify using MDA Subject Area if the calculated Fields for comparision calculation type with percentage is working fine when Fields A and B are from Show Fields")
+	public void testCalculatedFieldsUsingMatrixData() throws Exception {
+		
+		// Creating collection with data as source collection
+		CollectionInfo collectionInfo = mapper.readValue((new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4232/CollectionSchema.json")),CollectionInfo.class);
+		collectionInfo.getCollectionDetails().setCollectionName("GS-3432-1" + date.getTime());
+		String collectionId = gsDataImpl.createCustomObject(collectionInfo);
+		Assert.assertNotNull(collectionId, "Collection ID should not be null.");
+		CollectionInfo actualCollectionInfo = gsDataImpl.getCollectionMaster(collectionId);
+		String collectionName = actualCollectionInfo.getCollectionDetails().getCollectionName();
+		
+		JobInfo loadTransform = mapper.readValue((new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4232/DataloadJob.txt")), JobInfo.class);
+		File dataFile = FileProcessor.getDateProcessedFile(loadTransform, date);
+		DataLoadMetadata metadata = CollectionUtil.getDBDataLoadMetaData(actualCollectionInfo, DataLoadOperationType.INSERT);
+		Assert.assertTrue(gsDataImpl.isValidDataProvided(mapper.writeValueAsString(metadata), dataFile));
+		NsResponseObj nsResponseObj = gsDataImpl.loadDataToMDA(mapper.writeValueAsString(metadata), dataFile);
+		Assert.assertTrue(nsResponseObj.isResult());
+		
+		// Setting the collectionName value to the source object selection and others in input json
+		RulesPojo rulesPojo = mapper.readValue(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4232/GS-4232-Input.json"), RulesPojo.class);
+		LoadToSFDCAction loadToSFDCAction = mapper.readValue(rulesPojo.getSetupActions().get(0).getAction(), LoadToSFDCAction.class);
+		loadToSFDCAction.getFieldMappings().get(1).setSourceObject(collectionName);
+		rulesPojo.getSetupActions().get(0).setAction(mapper.convertValue(loadToSFDCAction, JsonNode.class));
+		rulesPojo.getSetupRule().setSelectObject(collectionName);
+		rulesPojo.getSetupRule().getSetupData().get(0).setSourceObject(collectionName);
+		Log.debug("Updated Pojo is" +mapper.writeValueAsString(rulesPojo));
+				
+		rulesManagerPage.openRulesManagerPage(rulesManagerPageUrl);
+		rulesManagerPage.clickOnAddRule();
+		rulesEngineUtil.createRuleFromUi(rulesPojo);
+		Assert.assertTrue(rulesUtil.runRule(rulesPojo.getRuleName()), "Check whether Rule ran successfully or not !");
+		
+		//Verifying the agrregated field vales with the expected data and actual aggregated data
+		dataETL.execute(mapper.readValue(resolveNameSpace(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4232/GS-4232-ExpectedJob.txt"),JobInfo.class));	
+		List<Map<String, String>> expectedData = Comparator.getParsedCsvData(new CSVReader(new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4232/ExpectedData.csv")));
+		List<Map<String, String>> actualData = Comparator.getParsedCsvData(new CSVReader(new FileReader(Application.basedir+ "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-4232/ActualData.csv")));
+		List<Map<String, String>> differenceData = Comparator.compareListData(expectedData, actualData);
+		Log.info("Actual : " + mapper.writeValueAsString(actualData));
+		Log.info("Expected : " + mapper.writeValueAsString(expectedData));
+		Log.info("Difference is : " + mapper.writeValueAsString(differenceData));
+		Assert.assertEquals(differenceData.size(), 0, "Check the Diff above for which the aggregated data is not matching");
+
+	}
+
+}
