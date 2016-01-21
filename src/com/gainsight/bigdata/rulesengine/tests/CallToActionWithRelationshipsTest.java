13cda1747dcf94f8c84ecb6a9a73aefcddd08675
Abhilash Thaduka <athaduka@gainsight.com>
2016-01-21 17:01:49 +0530

    Added testcases related to relationship cta's

--- src/com/gainsight/bigdata/rulesengine/tests/CallToActionWithRelationshipsTest.java
+++ src/com/gainsight/bigdata/rulesengine/tests/CallToActionWithRelationshipsTest.java
@@ -2,12 +2,18 @@ package com.gainsight.bigdata.rulesengine.tests;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import au.com.bytecode.opencsv.CSVReader;
 import com.gainsight.bigdata.NSTestBase;
+import com.gainsight.bigdata.dataload.enums.DataLoadOperationType;
+import com.gainsight.bigdata.dataload.pojo.DataLoadMetadata;
+import com.gainsight.bigdata.gsData.apiImpl.GSDataImpl;
+import com.gainsight.bigdata.pojo.CollectionInfo;
+import com.gainsight.bigdata.pojo.NsResponseObj;
 import com.gainsight.bigdata.rulesengine.RulesUtil;
 import com.gainsight.bigdata.rulesengine.pages.RulesConfigureAndDataSetup;
 import com.gainsight.bigdata.rulesengine.pages.RulesManagerPage;
@@ -15,25 +21,30 @@ import com.gainsight.bigdata.rulesengine.pages.SetupRulePage;
 import com.gainsight.bigdata.rulesengine.pojo.RulesPojo;
 import com.gainsight.bigdata.rulesengine.pojo.setupaction.CTAAction;
 import com.gainsight.bigdata.rulesengine.util.RulesEngineUtil;
+import com.gainsight.bigdata.tenantManagement.apiImpl.TenantManager;
+import com.gainsight.bigdata.tenantManagement.pojos.TenantDetails;
+import com.gainsight.bigdata.util.CollectionUtil;
 import com.gainsight.pageobject.core.Element;
-import com.gainsight.sfdc.administration.pages.AdminScorecardSection;
 import com.gainsight.sfdc.tests.BaseTest;
 import com.gainsight.sfdc.util.FileUtil;
 import com.gainsight.sfdc.util.datagen.DataETL;
+import com.gainsight.sfdc.util.datagen.FileProcessor;
 import com.gainsight.sfdc.util.datagen.JobInfo;
 import com.gainsight.testdriver.Application;
 import com.gainsight.testdriver.Log;
-
 import com.gainsight.util.Comparator;
+import com.gainsight.util.DBStoreType;
+import com.gainsight.util.MongoDBDAO;
 import com.gainsight.utils.annotations.TestInfo;
 import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 
 /**
  * Created by Abhilash Thaduka on 1/18/2016.
@@ -54,9 +65,17 @@ public class CallToActionWithRelationshipsTest extends BaseTest {
     private RulesConfigureAndDataSetup rulesConfigureAndDataSetup;
     private static final String CUSTOM_OBJECT_CLEANUP = "Delete [SELECT Id FROM C_Custom__c];";
     private DataETL dataETL = new DataETL();
+    private TenantManager tenantManager;
+    private Date date = Calendar.getInstance().getTime();
+    private TenantDetails tenantDetails = null;
+    private static DBStoreType dataBaseType;
+    GSDataImpl gsDataImpl = null;
+    private MongoDBDAO mongoDBDAO = null;
+    String collectionName;
 
     @BeforeClass
-    public void setUp() throws Exception {
+    @Parameters("dbStoreType")
+    public void setUp(@Optional String dbStoreType) throws Exception {
         basepage.login();
         nsTestBase.init();
         rulesManagerPageUrl = visualForcePageUrl + "Rulesmanager";
@@ -71,6 +90,46 @@ public class CallToActionWithRelationshipsTest extends BaseTest {
         sfdc.runApexCode(getNameSpaceResolvedFileContents(CREATE_RELATIONSHIP));
         JobInfo loadData = mapper.readValue((new FileReader(LOAD_DATA_INTO_CUSTOMOBJECT)), JobInfo.class);
         dataETL.execute(loadData);
+        gsDataImpl = new GSDataImpl(NSTestBase.header);
+        tenantManager = new TenantManager();
+        tenantDetails = tenantManager.getTenantDetail(null, tenantManager.getTenantDetail(NSTestBase.sfdc.fetchSFDCinfo().getOrg(), null).getTenantId());
+        if (dbStoreType != null && dbStoreType.equalsIgnoreCase(DBStoreType.MONGO.name())) {
+            Assert.assertTrue(tenantManager.disableRedShift(tenantDetails));
+        } else if (dbStoreType != null && dbStoreType.equalsIgnoreCase(DBStoreType.REDSHIFT.name())) {
+            Assert.assertTrue(tenantManager.enabledRedShiftWithDBDetails(tenantDetails));
+        } else if (dbStoreType != null && dbStoreType.equalsIgnoreCase(DBStoreType.POSTGRES.name())) {
+            dataBaseType = DBStoreType.valueOf(dbStoreType);
+            mongoDBDAO = new MongoDBDAO(NSTestBase.nsConfig.getGlobalDBHost(), Integer.valueOf(NSTestBase.nsConfig.getGlobalDBPort()), NSTestBase.nsConfig.getGlobalDBUserName(), NSTestBase.nsConfig.getGlobalDBPassword(), NSTestBase.nsConfig.getGlobalDBDatabase());
+            TenantDetails.DBDetail schemaDBDetails = null;
+            schemaDBDetails = mongoDBDAO.getSchemaDBDetail(tenantDetails.getTenantId());
+            if (schemaDBDetails == null || schemaDBDetails.getDbServerDetails() == null || schemaDBDetails.getDbServerDetails().get(0) == null) {
+                throw new RuntimeException("DB details are not correct, please check it.");
+            }
+            Log.info("Connecting to schema db....");
+            mongoDBDAO = new MongoDBDAO(schemaDBDetails.getDbServerDetails().get(0).getHost().split(":")[0], 27017, schemaDBDetails.getDbServerDetails().get(0).getUserName(), schemaDBDetails.getDbServerDetails().get(0).getPassword(), schemaDBDetails.getDbName());
+        }
+
+        // Loading testdata globally
+        boolean isLoadTestDataGlobally = true;
+        if (isLoadTestDataGlobally) {
+            CollectionInfo collectionInfo = mapper.readValue((new FileReader(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GlobalTestData/CollectionSchemaWithRelationshipMapping.json")), CollectionInfo.class);
+            collectionInfo.getCollectionDetails().setCollectionName(dbStoreType + date.getTime());
+            String collectionId = gsDataImpl.createCustomObject(collectionInfo);
+            Assert.assertNotNull(collectionId, "Collection ID should not be null.");
+            // Updating DB storage type to postgres from back end.
+            if (dbStoreType != null && dbStoreType.equalsIgnoreCase(DBStoreType.POSTGRES.name())) {
+                Assert.assertTrue(mongoDBDAO.updateCollectionDBStoreType(tenantDetails.getTenantId(), collectionId, DBStoreType.POSTGRES), "Failed while updating the DB store type to postgres");
+            }
+            CollectionInfo actualCollectionInfo = gsDataImpl.getCollectionMaster(collectionId);
+            collectionName = actualCollectionInfo.getCollectionDetails().getCollectionName();
+            dataETL.execute(mapper.readValue(resolveNameSpace(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GlobalTestData/ExtractJob.txt"), JobInfo.class));
+            JobInfo loadTransform = mapper.readValue((new FileReader(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GlobalTestData/DataloadJob.txt")), JobInfo.class);
+            File dataFile = FileProcessor.getDateProcessedFile(loadTransform, date);
+            DataLoadMetadata metadata = CollectionUtil.getDBDataLoadMetaData(actualCollectionInfo, DataLoadOperationType.INSERT);
+            Assert.assertTrue(gsDataImpl.isValidDataProvided(mapper.writeValueAsString(metadata), dataFile));
+            NsResponseObj nsResponseObj = gsDataImpl.loadDataToMDA(mapper.writeValueAsString(metadata), dataFile);
+            Assert.assertTrue(nsResponseObj.isResult(), "Data is not loaded, please check log for more details");
+        }
     }
 
     @BeforeMethod
@@ -93,7 +152,7 @@ public class CallToActionWithRelationshipsTest extends BaseTest {
 
     @TestInfo(testCaseIds = {"GS-8087", "GS-8090"})
     @Test(description = "testcase to create Relationship CTA using relationship object ")
-    public void createCtaUsingRelationshipObject() throws Exception {
+    public void createCtaUsingSfdcRelationshipObject() throws Exception {
         RulesPojo rulesPojo = mapper.readValue(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-8087/GS-8087-Input.json"), RulesPojo.class);
         rulesManagerPage.openRulesManagerPage(rulesManagerPageUrl);
         rulesManagerPage.clickOnAddRule();
@@ -120,7 +179,7 @@ public class CallToActionWithRelationshipsTest extends BaseTest {
 
     @TestInfo(testCaseIds = {"GS-8088"})
     @Test(description = "testcase to create Relationship CTA using linked object ")
-    public void createCtaUsingLinkedObject() throws Exception {
+    public void createCtaUsingSfdcLinkedObject() throws Exception {
         RulesPojo rulesPojo = mapper.readValue(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-8088/GS-8088-Input.json"), RulesPojo.class);
         rulesManagerPage.openRulesManagerPage(rulesManagerPageUrl);
         rulesManagerPage.clickOnAddRule();
@@ -143,5 +202,35 @@ public class CallToActionWithRelationshipsTest extends BaseTest {
         Assert.assertEquals(actualTasks.size(), 54, "Total Number of CSTasks are not matching for the cta's created");
         Log.info("Difference is : " + mapper.writeValueAsString(differenceData));
         Assert.assertEquals(differenceData.size(), 0, "Check the Diff above for which the CS-Tasks are not matching for the cta");
+    }
+
+
+    @TestInfo(testCaseIds = {"GS-200059"})
+    @Test(description = "testcase to create Relationship CTA using Mda linked collection")
+    public void createCtaUsingMdaLinkedCollection() throws Exception {
+        RulesPojo rulesPojo = mapper.readValue(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-TestData/GS-200059/GS-200059-Input.json"), RulesPojo.class);
+        rulesPojo.getSetupRule().setSelectObject(collectionName);
+        Log.debug("Updated testdata is " + mapper.writeValueAsString(rulesPojo));
+        rulesManagerPage.openRulesManagerPage(rulesManagerPageUrl);
+        rulesManagerPage.clickOnAddRule();
+        rulesEngineUtil.createRuleFromUi(rulesPojo);
+        Assert.assertTrue(rulesUtil.runRule(rulesPojo.getRuleName()), "Rule processing failed, Please check response object printed in log for more details !");
+        JsonNode action = rulesPojo.getSetupActions().get(0).getAction();
+        CTAAction ctaAction = mapper.readValue(action, CTAAction.class);
+        Assert.assertTrue(rulesUtil.isCTACreateSuccessfully(ctaAction.getPriority(), ctaAction.getStatus(), sfdcInfo.getUserId(), ctaAction.getType(), ctaAction.getReason(), ctaAction.getComments(), ctaAction.getName(), ctaAction.getPlaybook()), "verify whether cta action configured resulted correct cta or not");
+        Assert.assertEquals(sfdc.getRecordCount(resolveStrNameSpace(("select id, name FROM JBCXM__CTA__c where Name='" + ctaAction.getName() + "' and JBCXM__Source__c='Rules' and isdeleted=false"))), 9, "Total CTA's created are not matching");
+
+        //Verifying CS tasks of a CTA for the playbook applied
+        dataETL.execute(mapper.readValue(resolveNameSpace(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-Jobs/CSTasks.txt"), JobInfo.class));
+        JobInfo jobInfo = mapper.readValue(resolveNameSpace(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI-Jobs/playbookTasks.txt"), JobInfo.class);
+        CTAAction act = mapper.readValue(rulesPojo.getSetupActions().get(0).getAction(), CTAAction.class);
+        jobInfo.getExtractionRule().setWhereCondition(" where JBCXM__PlaybookId__r.Name='" + act.getPlaybook() + "'");
+        dataETL.execute(jobInfo);
+        List<Map<String, String>> expectedTasks = Comparator.getParsedCsvData(new CSVReader(FileUtil.resolveNameSpace(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI_ExpectedData/PlayBookTasks.csv"), null)));
+        List<Map<String, String>> actualTasks = Comparator.getParsedCsvData(new CSVReader(FileUtil.resolveNameSpace(new File(Application.basedir + "/testdata/newstack/RulesEngine/RulesUI_ExpectedData/CSTasks.csv"), null)));
+        List<Map<String, String>> differenceData = Comparator.compareListData(expectedTasks, actualTasks);
+        Assert.assertEquals(actualTasks.size(), 54, "Total Number of CSTasks are not matching for the cta's created");
+        Log.info("Difference is : " + mapper.writeValueAsString(differenceData));
+        Assert.assertEquals(differenceData.size(), 0, "Check the Diff above for which the CS-Tasks are not matching for the cta's");
     }
 }
