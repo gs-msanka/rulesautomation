a59737cf39bab6ce759d1ab54e15b5e7cdacaf95
Sanka Madhu Kiran <msanka@gainsight.com>
2016-04-01 17:44:52 +0530

    Changing mouseover action to doubleclick

--- src/com/gainsight/sfdc/pages/BasePage.java
+++ src/com/gainsight/sfdc/pages/BasePage.java
@@ -72,7 +72,7 @@ public class BasePage extends WebPage implements Constants {
 		}
 		field.clearAndSetText("username", sfdcConfig.getSfdcUsername());
 		field.clearAndSetText("password", sfdcConfig.getSfdcPassword());
-		element.mouseOverAndClickOnIdentifier("Login");
+		item.doubleClick("login");
         try {
             wait.waitTillElementPresent(USERNAVBUTTON, MIN_TIME, MAX_TIME);
         } catch (Exception e) {
